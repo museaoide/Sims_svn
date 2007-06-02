2b1259d00f65d8789002c44aa48a2325d034c1b4
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2007-06-02 01:11:17 +0000

    revisions 5/27-6/1/2007

--- toolbox/RIplus/trunk/screpDC.R
+++ toolbox/RIplus/trunk/screpDC.R
@@ -1,10 +1,13 @@
-setUpScrep <- function(crange=c(0.,0.5), nc,  pw, ww, U=function(x,y) {log(ifelse(x>0,x,1e-100)) + log(ifelse(y > x, y - x, 1e-100))}, lambda=1,
-                     DxU=function(x,y) {1/x - 1/(y-x)}, lbp=1e-7, PNLTY=1) {
-  ## ww,pw: Points of support and marginal probabilities for w distribution.  dw:wrange::dc:crange
+setUpScrep <- function(crange=c(0.,0.5), nc,  pw, ww, U=function(x,y) { log(pmax(x, .Machine$double.eps*2)) + log(pmax(y-x, .Machine$double.eps*2))}, kappa=1,
+                     DxU=function(x,y)  {ifelse(x > 0 & y>x, 1/x - 1/(y-x), 0)}, lbp=1e-7, PNLTY=1) {
+  ## Notes 5/28/2007:  derivative is not working?  needs to be made consistent in obf part with level fcn.  pdf emerges from csminwel not respecting c<w in first column.
+  ## Notes 6/1/2007:  Still need to fix derivative.  Seems possibly to be working, but numerically rough.
+  ## ----------------FIX ABOVE---------------------
+  ## ww,pw: Points of support and marginal probabilities for w distribution.  
   ## U:     objective function U(x,w). 
   ## DxU:   derivative of U w.r.t. its first argument. If this is NULL, numerical gradient is used.
   ## crange: range of values for x 
-  ## wrange: range of values for w 
+  ## nc:     number of distinct c values.  (length of dc)
   ## lambda: multiplier on the information constraint, utility cost of capacity
   ## lbp:    lower bound on elements of dc, pc, dw, pw and 1 - upper bound on their sums.
   ## PNLTY:  weight on penalty function keeping solution away from boundary.
@@ -20,16 +23,16 @@ setUpScrep <- function(crange=c(0.,0.5), nc,  pw, ww, U=function(x,y) {log(ifels
     ##
     if ((sum(dc) > 1) || any( dc < 0 ) ) {
       message("bad dc vector")
-      return(list(obf=-1e20))
+      return(list(obf=-1e100))
     }
     if ((sum(pc) > 1) || any (pc < 0 ) ) {
       message("bad pc vector")
-      return(list(obf=-1e20))
+      return(list(obf=-1e100))
     }
     cc <- cumsum(dc)
     pc <- c(pc, 1 - sum(pc))
     cc <- cc * (crange[2] - crange[1]) + crange[1]
-    cltw <- outer(cc, ww, function(a,b){a<b} )
+    cltw <- outer(cc, ww, function(a,b){a<b-.Machine$double.eps*2} )
     Umat <- outer(cc,ww, FUN=U)
     pdf <- exp(Umat)
     pdf <- pc * pdf
@@ -42,10 +45,11 @@ setUpScrep <- function(crange=c(0.,0.5), nc,  pw, ww, U=function(x,y) {log(ifels
     ## two lines below apply a penalty for pdf values near zero.  This prevents the search from wandering over large
     ## changes in large negative values of log p's, and may speed convergence.
     ##---------------------------------
-    zerop <- pdf[cltw & (pdf < lbp)]
+    zerop <- pdf[ cltw & (pdf < lbp)]
     zerodc <- dc[dc < lbp]
     if (sum(dc) > 1 - lbp ) zerodc <- c(zerodc, 1-sum(dc)) 
-    obf <- EU - lambda *k - PNLTY * (sum((lbp / zerop - 1)^4 ) + sum((lbp / zerodc -1)^4))
+    ## obf <- EU  - PNLTY * (sum((lbp / zerop - 1)^2 ) + sum((lbp / zerodc -1)^2) + 100 * sum(abs(rep(1,nc) %*% pdf - pw)) + 10 * max(k-kappa,0)^2)
+    obf <- EU  - PNLTY * (sum((lbp / zerop - 1)^2 ) + sum((lbp / zerodc -1)^2)  + 10 * max(k-kappa,0)^2)
     ##---------------------------------
     return(list(obf=obf, EU=EU, cc=cc, pc=pc, pdf=pdf, info=k/log(2)))
   }
@@ -84,7 +88,7 @@ setUpScrep <- function(crange=c(0.,0.5), nc,  pw, ww, U=function(x,y) {log(ifels
     DcU <- outer(cc,ww,DxU)
     pdeuh <- diag(pc) %*% (DcU * eU) %*% diag(h)
     peuh <- diag(pc) %*% eU %*% diag(h)
-    dobjdcv <- diag(pc) %*% DcU %*% h - lambda * ( apply(log(peuh) * pdeuh, 1, sum)  - pdeuh %*% log(apply(peuh,2,sum)) )
+    dobjdcv <- diag(pc) %*% DcU %*% h - lambda * ( apply(plogp(peuh), 1, sum)  - pdeuh %*% log(apply(peuh,2,sum)) )
     dobjdcv <- dobjdcv - PNLTY * (dc < lbp) * (-4) * (lbp/dc - 1)^3 * lbp / dc^2
     return(list(dp=dobjdpv, dc=dobjdcv, obf=obf, EU=EU, cc=cc, pc=pc, pdf=pdf, info=k/log(2)))
   }
@@ -96,7 +100,11 @@ setUpScrep <- function(crange=c(0.,0.5), nc,  pw, ww, U=function(x,y) {log(ifels
   DscrepDCS <- function(x) {
     dc <- x[1:nc]
     pc <- x[-(1:nc)]
-    return(c(-dobjdpdc$dc, dobjdpdc$dp[-nc]))
+    return(list(g=c(-dobjdpdc(dc,pc)$dc, -dobjdpdc(dc,pc)$dp[-nc]), badg=FALSE))
   }
+  ## since dobjdpdc also computes the function value, it would be more efficient to have csminwel
+  ## recognize this.  But since csminwel evaluates the gradient once, and the function often many times, on each
+  ## iteration, the efficiency gain might not be huge.
   return(list(screpDC=screpDC, DscrepDC=dobjdpdc, screpDCS=screpDCS, DscrepDCS=DscrepDCS))
 }
+plogp <- function(p) {p * log( ifelse(p>0, p,1)) }

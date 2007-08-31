d0277fe6c12aaef9ed868f6ecfafc725dd9ccee9
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2007-08-31 18:33:42 +0000

    Debug fixes for mleBC

--- toolbox/VARtools/trunk/PanelVAR/mleBC.R
+++ toolbox/VARtools/trunk/PanelVAR/mleBC.R
@@ -4,12 +4,11 @@ mleBC <- function(ydata, lags, SigFacs, mnprior=list(tight=.2,decay=.5),vprior=l
   nv <- dim(ydata)[[2]]
   nc <- dim(ydata)[[3]]
   vp <- varprior(nv=nv,nx=0,lags=lags, mnprior=mnprior,vprior=vprior)
-  browser()
   ndobs <- dim(vp$ydum)[1] - (length(vp$pbreaks)+1) * lags
   ydum <- rbind(vp$ydum[vp$pbreaks, ], vp$ydum[dim(vp$ydum)[1], ])
   ## line above works because each dummy observation takes up exactly lags+1 positions in vp$ydum.  With training
   ## sample component (as in rfvar3 or mgnldnsty), would need more logic.
-  xdum <- matrix(vp$ydum[-c(breaks, dim(vp$ydum)[1]), ], nrow=ndobs)
+  xdum <- matrix(vp$ydum[-c(vp$pbreaks, dim(vp$ydum)[1]), ], nrow=ndobs)
   ## now sum of coeffs dummy obs
   ## Unlike rfvar3.R, here we don't use initial conditions for these.  This might also be better in rfvar3.
   ## Now the observations are scaled by vprior$sig, which means that the mu weight on them must be much bigger here
@@ -24,13 +23,13 @@ mleBC <- function(ydata, lags, SigFacs, mnprior=list(tight=.2,decay=.5),vprior=l
   ## string out these dummy obs, because prior connection of cs to y0 below breaks
   ## I cross X structure of rhs.
   dim(ydum) <- c(dim(ydum)[1]*nv,1)
-  xdum <- t(Matrix(t(xdum), dim(xdum)[2], nv * dim(xdum)[1])) # stacks up vertically nv copies of xdum
+  xdum <- t(Matrix(as.vector(t(xdum)), dim(xdum)[2], nv * dim(xdum)[1])) # stacks up vertically nv copies of xdum
   ## Get actual data in regression form
   y <- Matrix(ydata[-(1:lags), , ], ncol=1)
   x <- array(0, c(nT-lags, nv, lags, nc))
   for (il in 1:lags)
     x[ , , il, ] <- ydata[(lags+1):nT - il, , ]
-  X <- NULL
+  X <- Matrix(0, nrow=0, ncol = nv * nv * lags)
   for (ic in 1:nc)
     X <- rBind(X, kronecker(Diagonal(nv), Matrix(x[ , , , ic], nrow = nT - lags))) #each variable in a country gets the same
                                         #matrix of rhs lagged y's
@@ -46,7 +45,7 @@ mleBC <- function(ydata, lags, SigFacs, mnprior=list(tight=.2,decay=.5),vprior=l
   ## Now add rows that connect constants to y0's
   y0 <- Matrix(ydata[1:lags, , ], ncol=1) - Matrix(Mu, lags * nv * nc, ncol=1) #subtracts out the mu from y0=Lambda*cs+mu
   x0 <- Matrix(0, lags*nv*nc, dim(X)[2])
-  x0[ , lags * nv + 1:(nc * nv)] <- kronecker(Diagonal(nc), Lambda)
+  x0[ , lags * nv *nv + 1:(nc * nv)] <- kronecker(Diagonal(nc), Lambda)
   X <- rBind(X, x0)
   y <- rBind(y,y0)
   ## prior on country-variable constants
@@ -66,7 +65,7 @@ mleBC <- function(ydata, lags, SigFacs, mnprior=list(tight=.2,decay=.5),vprior=l
   if (!is.null(csigFac)) {
     csigNdx <- max(OmNdx) + 1:(nv * nc)
   }
-  BigFac[csigNdx, csigNdx] <- csig
+  BigFac[csigNdx, csigNdx] <- kronecker(Diagonal(nc), csigFac)
   stopifnot (max(csigNdx) == dim(BigFac)[2])  #This line can come out once things are running smoothly
   qrX <- qr(BigFac %*% X)
   stdy <- BigFac %*% y

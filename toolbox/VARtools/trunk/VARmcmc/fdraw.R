5f073489caf0a56d1a2e80866d59be8af929a947
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2007-08-31 11:11:27 +0000

    making fdraw match before deletion

--- toolbox/VARtools/trunk/VARmcmc/fdraw.R
+++ toolbox/VARtools/trunk/VARmcmc/fdraw.R
@@ -1,9 +1,12 @@
-fdraw <- function(parvec, S, df, ndraw, T) {
-  U <- solve(S)
-  V <- rwwish(df, df * U / T, ndraw)
-  pv <- dwish(crossprod(V), df, df * U/T)
-  pard <- solve(V, rnorm(length(pard)))
-  return(pard)
-}
-## Not sure what this is supposed to be doing.  Obviously incomplete 
-## (pard undefined, parvec unused)
+fdraw <- function(y0, By, Bx, xdata=NULL, const=TRUE, horiz, xxi, smat, df, ndraw) {
+  scaledraw <- rvecwish(df, cs=t(smat), ndraw=ndraw)
+  nv <- dim(By)[1]
+  shocks <- rnorm(nv*nv*horiz*ndraw)
+  shocks <- array(shocks, c(nv,nv,horiz,ndraw))
+  for (draw in 1:ndraw) {
+    shocks <- tensor(scaledraw[ , , draw], shocks[ , , ,draw], 1, 1)
+  }
+  lags <- dim(y0)[[1]]
+  yf <- array(0, nv, horiz, ndraw)  * here need to duplicate logic of fcast, but with shocks, and vectorized over draws
+  for (it in 1:horiz)
+    yf[ , 

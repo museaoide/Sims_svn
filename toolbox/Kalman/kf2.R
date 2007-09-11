6b07751985f97712f323fcf7dcc399595a962a16
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2007-09-11 18:18:37 +0000

    Alternate Kalman filter.  Should be faster for long state vectors.

---
+++ toolbox/Kalman/kf2.R
@@ -0,0 +1,50 @@
+kf2 <- function(y,H,shat,sig,G,M) {
+  ## s is the state, and the plant equation is s(t)=G %*% s(t-1)+t(M) %*% e, where e is
+  ## N(0,I).  The observation equation is y(t)=Hs(t).  The prior distribution for
+  ## s is N(shat,sig).  To handle the standard Kalman Filter setup where the observation
+  ## equation is y(t)=Hs(t)+Nu(t), expand s to become [s;v] (where v=Nu), expand H to [H I], replace
+  ## G by [G 0;0 0], and replace M with [M 0;0 N].  The resulting model has no "error" in the
+  ## observation equation but is equivalent to the original model with error in the state equation.
+  ## The posterior on the new state is N(shatnew,signew) and lh is a two-dimensional vector containing
+  ## the increments to the two component terms of the log likelihood function.  They are added 
+  ## to form the log likelihood, but are used separately in constructing a concentrated or marginal
+  ## likelihood. yhat is the error in the forecast of y based on the prior.
+  SMALLSV <- 1e-10
+  omega <- G %*% sig %*% t(G) + crossprod(M)
+  if (is.null(dim(H))) dim(H) <- c(1,length(H))
+  nobs <- dim(H)[1]
+  nstate <- dim(H)[2]
+  ## stopifnot (nstate >= nobs)
+  if (isTRUE(all.equal(H, 0))) { # No observation case.  Just propagate the state.
+    lh <- c(0,0)
+    shatnew <- G %*% shat
+    signew <- omega
+    fcsterr <- y                        # y had better be 0
+    if (!all.equal(y,0) ) warning("zero H but non-zero y")
+  } else {   
+    svdhoh <- svd( H %*% omega %*% t(H) )
+    if (all(svdhoh$d < SMALLSV)) { # Observation is uninformative.  Again propagate state.
+      lh <- c(0,0)
+      shatnew <- G %*% shat
+      signew <- omega
+      fcsterr <- y - H %*% G %*% shat            # had better be 0
+      if (!isTRUE(all.equal(y,0)) ) warning("Uninformative H but non-zero fcsterr")
+    } else {
+      first0 <- match(TRUE, svdhoh$d < SMALLSV)
+      if (is.na(first0)) first0 <- min(dim(H))+1
+      u <- svdhoh$u[ , 1:(first0-1), drop=FALSE]
+      v <- svdhoh$v[ , 1:(first0-1), drop=FALSE]
+      d <- svdhoh$d[1:(first0-1), drop=FALSE]
+      fcsterr <- y-H %*% G %*% shat
+      ho <- H %*% omega
+      hohifac <-diag(1/sqrt(d)) %*% t(u)
+      ferr <- hohifac %*% fcsterr
+      lh <- c(0,0)
+      lh[1] <- -.5 * crossprod(ferr)
+      lh[2] <- -.5 * sum( log(d) )
+      shatnew <- t(ho) %*% t(hohifac) %*% ferr + G %*% shat
+      signew <- omega - t(ho) %*% crossprod(hohifac) %*% ho
+    }
+  }
+  return(list(shatnew=shatnew, signew=signew, lh=lh, fcsterr=fcsterr))
+}

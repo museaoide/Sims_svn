9618d7f128fa19f5c81e6dca1a370f9eea220a4d
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2008-02-17 03:49:33 +0000

    make g0g1 return expressions, not functions

--- toolbox/gensys/trunk/R/g0g1.R
+++ toolbox/gensys/trunk/R/g0g1.R
@@ -11,18 +11,18 @@ g0g1 <- function(ex,x,xl,shock,experr,param){
   Phi <- list(0)
   Pi <- list(0)
   for(ix in 1:nf){
-    g0[[ix]] <- deriv(ex[ix],x,function.arg=c(x,xl,shock,experr,param) )
-    g1[[ix]] <- deriv(ex[ix],xl,function.arg=c(x,xl,shock,experr,param))
-    Phi[[ix]] <- deriv(ex[ix],shock,function.arg=c(x,xl,shock,experr,param))
-    Pi[[ix]] <- deriv(ex[ix],experr,function.arg=c(x,xl,shock,experr,param))
+    g0[[ix]] <- deriv(ex[ix],x) #,function.arg=c(x,xl,shock,experr,param) )
+    g1[[ix]] <- deriv(ex[ix],xl) #,function.arg=c(x,xl,shock,experr,param))
+    Phi[[ix]] <- deriv(ex[ix],shock) #,function.arg=c(x,xl,shock,experr,param))
+    Pi[[ix]] <- deriv(ex[ix],experr) #,function.arg=c(x,xl,shock,experr,param))
   }
   names(g0) <- names(ex)
   names(g1) <- names(ex)
   names(Phi) <- names(ex)
   names(Pi) <- names(ex)
-  ## What is returned is a list of 4 lists of nf functions.  The values the functions returns
-  ## are those of the expressions in ex, but the values also have "gradient" attributes, so
-  ## that attr(function-called-with-non-symbolic-arguments,"gradient") is the gradient vector,
+  ## What is returned is a list of 4 lists of nf expressions.  When evaluated, these
+  ## expressions yield the values of the expressions in ex, but they also have "gradient" attributes, so
+  ## that attr(evaluated expression,"gradient") is the gradient vector,
   ## or a gradient matrix if the elements of x (say) are vectors.  Note that when this routine
   ## returns a matrix as the gradient, this is not the df/dx matrix with f the ex vector.  It is
   ## df(ix)/dx for a particular ix, evaluated at a vector of arguments.

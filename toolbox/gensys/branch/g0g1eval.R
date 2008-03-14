56b9c25dcdc55243533fd28b0d352dce170e6b47
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2008-03-14 20:50:03 +0000

    Further work on g0g1eval, add makessf

--- toolbox/gensys/branch/g0g1eval.R
+++ toolbox/gensys/branch/g0g1eval.R
@@ -14,7 +14,7 @@ g0g1eval <- function(dexpr, x, xl, nshock, experr, param) {
   Psi <- matrix(0, nex, nshock)
   Pi <- matrix(0, nex, length(experr))
   for ( i in 1:nex ) {
-    dvec <- attr(eval(dexpr[i], as.list(c(xx=xx,xl=xl,shock=rep(0,nshock),param=param))),"gradient")
+    dvec <- attr(eval(dexpr[i], as.list(c(x=x,xl=xl,shock=rep(0,nshock),param=param))),"gradient")
     loc <- 0
     g0[i,] <- dvec[1:nx]
     loc <- nx

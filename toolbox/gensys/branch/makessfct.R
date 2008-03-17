6bf3a2a49cefc25d03149f51223b05ff57532ff7
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2008-03-17 14:48:07 +0000

    makessfct added (cont time version of  makessf)

---
+++ toolbox/gensys/branch/makessfct.R
@@ -0,0 +1,19 @@
+makessfct <- function(eq, xnames, dxnames=paste(xnames,"dot",sep=""), shocknames, paramnames, paramv){
+  ## creates a steady-state function with a single numeric vector argument, with the fixed parameter
+  ## and (zero) shock values stored in an environment within this makessf function.
+  eqev <- vector("numeric", 2*length(xnames)+length(shocknames)+length(paramnames))
+  names(eqev) <- c(xnames,dxnames,shocknames,paramnames)
+  eqev[paramnames] <- paramv
+  eqev[shocknames] <- 0
+  eqev <- as.list(eqev)
+  ssf <- function(xv) {
+    fval <- vector("numeric",length(eq))
+    eqev[xnames] <- xv
+    eqev[xdotnames] <- 0
+    for (iq in 1:length(eq)) {
+      fval[iq] <- eval(eq[iq],eqev)
+    }
+    return(fval)
+  }
+  return(ssf)
+}

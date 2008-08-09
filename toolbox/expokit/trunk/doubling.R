3c859e404ce57696b287ca16e24d750af5dabb59
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2008-08-09 17:45:23 +0000

    final stage of system differentiation move, add doubling.R to expokit, SecondOrderLH to VARtools

---
+++ toolbox/expokit/trunk/doubling.R
@@ -0,0 +1,18 @@
+doubling <- function(A,omega,crit=1e-9) {
+  V <- omega
+  Aj <- A
+  j <- 1
+  vinc <- sum(abs(V))
+  while (j < 10000 && vinc > crit ) {
+    dv <-  Aj %*% V %*% t(Aj)
+    vinc <- sum(abs(dv))
+    V <- V + dv
+    Aj <- Aj %*% Aj
+    j <- j+1
+  }
+  ## print(j)
+  ## print(sum(abs(dv)))
+  if ( vinc > crit) warning("unconverged doubling")
+  return(V)
+}
+    

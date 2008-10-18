0e594485884d5f5ccc011b649474d6eac4fffb57
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2008-10-18 01:56:45 +0000

    ksmooth.R now handles singular state vcv correctly. numgrad.R back to standard delta. add Siginit2 and blkOrder

--- toolbox/Kalman/ksmooth.R
+++ toolbox/Kalman/ksmooth.R
@@ -13,8 +13,10 @@ ksmooth <- function(btt,Stt,bnT,SnT,A,omega) {
   ## cases where part or all of the state vector is known with certainty.
   svdg <- svd(G)
   di <- svdg$d
-  nzndx <- di > sqrt(.Machine$double.eps)
+  ##nzndx <- di > sqrt(.Machine$double.eps)
+  nzndx <- di > .Machine$double.eps
   di[nzndx] <- 1/di[nzndx]
+  di[-nzndx] <- 0
   SAGI <- t(AS) %*% svdg$u %*% diag(di) %*% t(svdg$v)
   ##SAGI <- t(qr.solve(t(G),AS))
   ##*****************************

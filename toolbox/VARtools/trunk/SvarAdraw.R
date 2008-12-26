d810205f557af9b43f1a291c5ac24eef9da36cd4
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2008-12-26 22:48:22 +0000

    First working (?) SvarDraw.R.  Add notes on SVARIR and blkDglz

--- toolbox/VARtools/trunk/SvarAdraw.R
+++ toolbox/VARtools/trunk/SvarAdraw.R
@@ -1,4 +1,15 @@
-SvarAdraw <- function(Ahat, idmat, Sighat) {
+SvarAdraw <- function(Ahat, idmat, Sighat, SigDraws) {
   n <- dim(Ahat)[1]
-  pndx <- seq(1, n^2, by=n) %% n
-  
+  pndx <- c(t(matrix(1:(n^2),n,n)))     #permutation for vec(t())
+  idndx <- (1:n^2)[idmat != 0]
+  ndraw <- dim(SigDraws)[3]
+  IAAI <- kronecker(A,diag(n)) + kronecker(diag(n), t(A))
+  IAAI <- qr(IAAI)[ , idndx]
+  dA <- qr.solve(IAAI, c(matrix(SigDraws, n^2, ndraw) - matrix(SigHat, n^2, ndraw)))
+  Ad < matrix(dA, ncol=ndraw) + Ahat
+  Adraw <- matrix(0, n^2, ndraw)
+  Adraw[idnx, ] <- dA + c(Ahat)
+  Adraw <- array(Adraw, c(n, n, ndraw))
+  return(Adraw)
+}
+    

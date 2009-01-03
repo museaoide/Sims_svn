b8a4b02a4191f42a0ec954315eed7b70cba03c89
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2009-01-03 19:12:31 +0000

    parseable version of ScrepDCD code, still needing work

--- toolbox/RIplus/trunk/screpDCD.R
+++ toolbox/RIplus/trunk/screpDCD.R
@@ -1,9 +1,10 @@
-setUpScrep <- function(crange=c(0.,0.5), nc,  pw, ww,
+setUpScrep <- function(crange=c(0.,0.5), pw, ww,
                        U=function(x,y) { log(pmax(x, .Machine$double.eps*2)) + log(pmax(y-x, .Machine$double.eps*2))}, kappa=1,
                      DxU=function(x,y)  {ifelse(x > 0 & y>x, 1/x - 1/(y-x), 0)}, lbp=1e-7, PNLTY=1) {
-  ## Notes 5/28/2007:  derivative is not working?  needs to be made consistent in obf part with level fcn.
-  ## pdf emerges from csminwel not respecting c<w in first column.
-  ## Notes 6/1/2007:  Still need to fix derivative.  With numgrad seems possibly to be working, but numerically rough.
+  ## Notes 1/3/09:  Need to distinguish systematically between "compacted" derivative terms and others.  E.g., when x <- diagmul.tensor(h, "iw", eaU),
+  ## then dxdh compacted is simply eaU, whereas the full matrix version would be a 3-level tensor with zeros everywhere that iw has different values in
+  ## h and eaU.  There may be places below where compacted derivatives are used as if not compacted, and probably some places where compacted derivatives
+  ## could be used where they are not.
   ## ----------------FIX ABOVE---------------------
   ## ww,pw: Points of support and marginal probabilities for w distribution.  
   ## U:     objective function U(x,w). 
@@ -33,162 +34,183 @@ setUpScrep <- function(crange=c(0.,0.5), nc,  pw, ww,
       message("bad pc vector")
       return(list(obf=-1e100))
     }
+    nc <- length(dc)
     cc <- to.tensor(cumsum(dc),dims=c(ic=nc))
-    pc <- to.tensor(c(pc, 1 - sum(pc), dims=c(ic=nc)))
+    pc <- to.tensor(c(pc, 1 - sum(pc)), dims=c(ic=nc))
     cc <- cc * (crange[2] - crange[1]) + crange[1]
     ww <- to.tensor(ww,dims=c(iw=nw))
-    pw <- to.tensor(pw,dim=c(iw=nw))
+    pw <- to.tensor(pw,dims=c(iw=nw))
     ## cltw <- outer(cc, ww, function(a,b){a<b-.Machine$double.eps*2} )
-    Umat <- to.tensor(outer(cc, ww, FUN=U), dims=c(dim(cc), dim(ww)), what=c(1,2))
+    Umat <- as.tensor(outer(as.vector(cc), as.vector(ww), FUN=U), dims=c(dim(cc), dim(ww)))
     ## deriv---------------
-    dUmatdcc <- to.tensor(0,c(ic=nc, iw=nw, idc=nc))
-    for (i in 1:nc) dUmatdcc[i, , i] <- DxU(cc[i],ww)
+    dUmatdcc <- as.tensor(outer(as.vector(cc),as.vector(ww), FUN=DxU), dims=c(dim(cc), dim(ww))) #compacted
+    ## dUmatdcc <- to.tensor(0,c(ic=nc, iw=nw, idc=nc))
+    ## for (i in 1:nc) dUmatdcc[i, , i] <- DxU(cc[i],ww)
     ## /deriv---------------
     eaU <- exp(alpha * Umat)
     ## deriv----------------
-    deaUdcc <- rep(eaU, nc, pos=3, name="idc")
-    deaUdcc <- deaUdcc * dUmatdcc * alpha
+    deaUdcc <- eaU * dUmatdcc * alpha   #compacted
+    ## deaUdcc <- rep(eaU, nc, pos=3, name="idc")
+    ## deaUdcc <- deaUdcc * dUmatdcc * alpha
     deaUdalpha <- eaU * Umat
     ## /deriv---------------
-    peaU <- mul.tensor(X=pc, Y=eaU, by="ic") 
+    peaU <- diagmul.tensor(pc, "ic", eaU) 
     ## deriv----------------
-    dpeaUdpc <- to.tensor(0, c(ic=nc, iw=nw, idc=nc))
-    for (i in 1:nc) dpeaUdpc[i, , i] <- eaU[i, ]
-    dpeaUdcc <- mul.tensor(X=pc, Y=deaUdcc, by="ic")
+    dpeaUdpc <- eaU                     #compacted
+    ## dpeaUdpc <- to.tensor(0, c(ic=nc, iw=nw, idc=nc))
+    ## for (i in 1:nc) dpeaUdpc[i, , i] <- eaU[i, ]
+    dpeaUdcc <- diagmul.tensor(pc, "ic", deaUdcc) #compacted
     dpeaUdalpha <- peaU * Umat
     ## /deriv---------------
     pwhat <- margin.tensor(peaU, "ic")
     ## deriv----------------
-    dpwhatdcc <- margin.tensor(dpeaUdcc, "ic")
-    dpwhatdpc <- margin.tensor(dpeaUdpc, "ic")
+    ## the first two of these, below are compacted:  cc[i] and pc[i] affect only column i of peaU
+    dpwhatdcc <- dpeaUdcc # compacted
+    dpwhatdpc <- dpeaUdpc #compacted
     dpwhatdalpha <-  margin.tensor(dpeaUdalpha,"ic")
     ## /deriv---------------
-    h <- pw/pwhat
+    h <- pw/pwhat                       
     ## deriv---------------
-    dhdpwhat <-  -h/pwhat
-    dhdcc <- mul.tensor(dhdpwhat, Y=dpwhatdcc, by="iw")
-    dhdpc <- mul.tensor(dhdpwhat * dpwhatdpc
-    dhdalpha <- dhdpwhat * dpwhatdalpha
+    dhdpwhat <-  -h/pwhat               #compacted.
+    dhdcc <- diagmul.tensor(dhdpwhat, "iw", dpwhatdcc)
+    dhdpc <- diagmul.tensor(dhdpwhat, "iw", dpwhatdpc)
+    dhdalpha <- mul.tensor(dhdpwhat, "iw", dpwhatdalpha)
     ## /deriv--------------
-    pdf <- mul.tensor(h, NULL, peaU, NULL, by="iw")
+    pdf <- diagmul.tensor(h, "iw", peaU)
     ## deriv--------------
-    dpdfdh <- to.tensor(0, c(ic=nc, iw=nw, idw=nw))
-    for (j in 1:nw) dpdfdh[ ,j, j] <- peaU[,j]
-    dpdfdpc <- to.tensor(0,c(ic=nc,iw=nw,idc=nc))
-    eaUh <- mul.tensor(h, NULL, eaU, NULL, by="iw")
-    for (i in 1:nc) dpdfdpc[i, , i] <- eaUh[i, ]
-    dpdfdpc <- dpdfdpc + mul.tensor(dpdfdh, "idw", dhdpc, "iw")
-    dpdfdcc <- to.tensor(0,c(ic=nc, iw=nw, idc=nc))
-    for (i in 1:nc) dpdfdcc[i, , i] <- 
-    dpdfdalpha <- mul.tensor(dhdalpha, NULL, peaU, NULL, by="iw") + mul.tensor(h, NULL, dpeaUdalpha, NULL, by="iw")
-    # #t(c(dhdalpha) * t(peaU)) + t(h * t(dpeaUdalpha)) 
+    dpdfdh <- peaU                      #compacted
+    ## dpdfdh <- to.tensor(0, c(ic=nc, iw=nw, idw=nw))
+    ## for (j in 1:nw) dpdfdh[ic=1:nc, iw=j, idw=j] <- peaU[ic=1:nc, iw=j]
+    ##!! forgot the need to recognize that only diagonal elements are non-zero and messed up some lines below.
+    ## dpdfdpc <- to.tensor(0,c(ic=nc,iw=nw,idc=nc))
+    ## eaUh <- diagmul.tensor(h, "iw", eaU)
+    dpdfdpc <- diagmul.tensor(h, "iw", dpeaUdpc) #compacted
+    dpdfdpc <- dpdfdpc + diagmul.tensor(dhdpc, "iw", peaU) #compacted
+    ## for (i in 1:nc) dpdfdpc[ic=i,iw=1:nw , idc=1:nc] <- eaUh[ic=i, iw=1:nw]
+    ## dpdfdpc <- dpdfdpc + mul.tensor(dpdfdh, "idw", dhdpc, "iw")
+    dpdfdcc <- diagmul.tensor(dhdcc, "iw", peaU) + diagmul.tensor(h, "iw", dpeaUdcc)
+    dpdfdalpha <- diagmul.tensor(dhdalpha,"iw", peaU) + diagmul.tensor(h, "iw", dpeaUdalpha)
+                                        # #t(c(dhdalpha) * t(peaU)) + t(h * t(dpeaUdalpha)) 
     ## /deriv---------------
-    ##!! still need to add deriv sections for redefined h and renormed pw, plus for penalty terms.
-    h <- margin.tensor(pdf, "ic")     #rep(1,nc) %*% pdf 
-    h <- pw / h
-    pdf <- mul.tensor(h, NULL, pdf, NULL, by="iw")   #t(c(h) * t(pdf ))
+    h0 <- margin.tensor(pdf, "ic")
+    ## deriv----------------------
+    dh0dcc <- margin.tensor(dpdfdcc,"ic")
+    dh0dpc <- margin.tensor(dpdfdpc, "ic")
+    dh0dalpha <- margin.tensor(dpdfdalpha, "ic")
+    ## /deriv----------------------
+    h <- pw / h0
+    ## deriv----------------------
+    dhdcc <- diagmul.tensor(-(h / h0 ), "iw", dh0dcc)
+    dhdpc <- diagmul.tensor(-(h / h0 ), "iw", dh0dpc)
+    dhdalpha <- diagmul.tensor(-(h/h0), "iw", dh0dalpha)
+    ## /deriv-------------------------------
+    pdf <- diagmul.tensor(h, "iw", pdf) #t(c(h) * t(pdf ))
+    ## deriv--------------------------------
+    dpdfdcc <- diagmul.tensor(dhdcc, "iw", pdf ) + diagmul.tensor(h, "iw", dpdfdcc)
+    dpdfdpc <- diagmul.tensor(dhdpc, "iw", pdf) + diagmul.tensor(h, "iw", dpdfdpc)
+    dpdfdalpha <- diagmul.tensor(dhdalpha, "iw", pdf) + diagmul.tensor(h, "iw", dpdfdalpha)
+    ## /deriv--------------
     EU <- sum(pdf * Umat)
-    k <- sum(pdf * log(ifelse(pdf>0,pdf,1))) - sum(pc * log(ifelse(pc>0,pc,1))) - sum(pw * log(ifelse(pw > 0, pw, 1)))
+    ## deriv-----------------
+    dEUdcc <- margin(diagmul.tensor(dpdfdcc, names(Umat), Umat), names(Umat))
+    dEUdpc <- margin(diagmul.tensor(dpdfdpc, names(Umat), Umat), names(Umat))
+    dEUdalpha <- margin(diagmul.tensor(dpdfdalpha, names(Umat), Umat), names(Umat))
+    ## /deriv------------------
+    ## k <- sum(pdf * log(ifelse(pdf>0,pdf,1))) - sum(pc * log(ifelse(pc>0,pc,1))) - sum(pw * log(ifelse(pw > 0, pw, 1)))
+    ## paranoid version above should not be needed with lbp, and version below makes deriv easier.
+    k <- sum(pdf * log(pdf)) - sum(pc * log(pc)) - sum(pw * log(pw))
+    ## deriv-------------------------
+    dkdcc <- margin(diagmul.tensor(dpdfdcc, names(pdf), log(pdf) ) + dpdfdcc, names(pdf))
+    dkdpc <- margin(diagmul.tensor(dpdfdpc, names(pdf), log(pdf) ) + dpdfdpc, names(pdf)) - log(pc)[[ic=~idp]] - one.tensor(c(idp=nc))
+    dkdalpha <- margin(diagmul.tensor(dpdfdalpha, names(pdf), log(pdf) ) + dpdfdalpha, names(pdf))
+    ## /deriv--------------------
     ## obf <- EU - lambda * k
     ## two lines below apply a penalty for pdf values near zero.  This prevents the search from wandering over large
     ## changes in large negative values of log p's, and may speed convergence.
     ##---------------------------------
-    zerop <- pdf[ cltw & (pdf < lbp)]
-    ## zerodc <- dc[dc < lbp]
-    ## if (sum(dc) > 1 - lbp ) zerodc <- c(zerodc, 1-sum(dc))
-    zerodc <- dc[dc<0]
-    if(sum(dc) >1) zerodc <- c(zerodc, 1-sum(dc))
+    ## zerop <- pdf[ (pdf < lbp)]          #sum(pdf) is one by construction, so don't penalize its getting close to one.
+    zerop <- pc[ pc < lbp ]
+    if (sum(pc) > 1 - lbp ) zerop <- c(zerop, 1-sum(pc))
+    zerodc <- dc[dc < lbp]
+    if (sum(dc) > 1 - lbp ) zerodc <- c(zerodc, 1-sum(dc))
+    zerodc <- to.tensor(zerodc, c(zc=length(zerodc)))
+    ## deriv------------------------
+    zerop <- to.tensor(zerop, c(izp=length(zerop)))
+    zerodc <- to.tensor(zerodc, c(izc=length(zerodc)))
+    ddcdcc <- toeplitz(nc:1)
+    ddcdcc[lower.tri(ddcdcc)] <- 0
+    ddcdcc <- solve(ddcdcc)
+    ddcdcc <- as.tensor(ddcdcc, dims=c(iddc=nc,idc=nc))
+    dzeropdp <- delta.tensor(c(idp=nc))$izp.idp
+    dzeropdp <- dzeropdp[ izp= (pc < lbp), idp=1:nc, drop=FALSE]
+    dzerocdcc <- delta.tensor(c(iddc=nc))$izc.iddc
+    dzerocdcc <- dzerocdcc[ izc= (dc < lbp), iddc=1:nc, drop=FALSE]
+    dzerocdcc <- mul.tensor(dzerocdcc, "iddc", ddcdcc, "iddc")
+    ## /deriv------------------------
+    ## zerodc <- dc[dc < 0]
+    ## if(sum(dc) >1) zerodc <- c(zerodc, 1-sum(dc))
     ## obf <- EU  - PNLTY * (sum((lbp / zerop - 1)^2 ) + sum((lbp / zerodc -1)^2) + 100 * sum(abs(rep(1,nc) %*% pdf - pw)) + 10 * max(k-kappa,0)^2)
     ##obf <- EU  - PNLTY * (sum((lbp / zerop - 1)^2 ) + sum((lbp / zerodc -1)^2)  + 10 * max(k-kappa,0)^2)
     obf <- EU  - PNLTY * (sum((lbp / zerop - 1)^2 ) + sum(zerodc^2)  + 10 * max(k-kappa,0)^2)
+    ## deriv-------------------------------
+    dobfdcc <- dEUdcc - PNLTY * (mul.tensor(2 * (lbp / zeroc -1) * (-lbp / zeroc^2), "izc", dzerocdcc, "izc") + 10 * (k > kappa) * 2 * (k - kappa) * dkdcc)
+    dobfdpc <- dEUdpc - PNLTY * (mul.tensor(2 * (lbp / zerop -1) * (-lbp / zerop^2), "izp", dzeropdp,  "izp") + 10 * (k > kappa) * 2  * (k - kappa) * dkdpc)
+    dobfdalpha <- dEUdalpha - PNLTY * (10 * (k > kappa) * 2 * (k - kappa) * dkdalplha)
+    ## /deriv---------------------------------
+    return(list(obf=obf, EU=EU, cc=cc, pc=pc, pdf=pdf, info=k/log(2), dobfdcc, dobfdpc, dobfdalpha))
+  }
+  screpDCO <- function(dc=rep(.1,9), pc=rep(.111,8), alpha=1) {
+    ## computes objective function, including the penalty defining the constraints.
+    ## dc:    determines the points in crange that form the support of the c distribution.  The entries are all positive and
+    ##        sum to one.  Support point i is at (sum_1^i dc_i) * (crange[2] - crange[1]) + crange[1].
+    ## pc:    Marginal probabilities on the points in c's support, with last one omitted ( = 1 - sum of others).
+    ## alpha: 1/(Lagrange multiplier on info constraint).  This is treated as a free parameter, so that the FOC's
+    ##        let us write the joint density as pc %*% exp(alpha*U) %*% mu(w) for a pdf pc and a weighting function mu.
+    require("tensorA")
+    if ((sum(dc) > 1) || any( dc < 0 ) ) {
+      message("bad dc vector")
+      return(list(obf=-1e100))
+    }
+    if ((sum(pc) > 1) || any (pc < 0 ) ) {
+      message("bad pc vector")
+      return(list(obf=-1e100))
+    }
+    nc <- length(dc)
+    cc <- to.tensor(cumsum(dc),dims=c(ic=nc))
+    pc <- to.tensor(c(pc, 1 - sum(pc)), dims=c(ic=nc))
+    cc <- cc * (crange[2] - crange[1]) + crange[1]
+    ww <- to.tensor(ww,dims=c(iw=nw))
+    pw <- to.tensor(pw,dims=c(iw=nw))
+    ## cltw <- outer(cc, ww, function(a,b){a<b-.Machine$double.eps*2} )
+    Umat <- as.tensor(outer(as.vector(cc), as.vector(ww), FUN=U), dims=c(dim(cc), dim(ww)))
+    eaU <- exp(alpha * Umat)
+    peaU <-diagmul.tensor(pc, "ic", eaU) 
+    pwhat <- margin.tensor(peaU, "ic")
+    h <- pw/pwhat
+    pdf <- diagmul.tensor(h, "iw", peaU)
+    h0 <- margin.tensor(pdf, "ic")
+    h <- pw / h0
+    pdf <- diagmul.tensor(h, "iw", pdf) #t(c(h) * t(pdf ))
+    EU <- sum(pdf * Umat)
+    ## k <- sum(pdf * log(ifelse(pdf>0,pdf,1))) - sum(pc * log(ifelse(pc>0,pc,1))) - sum(pw * log(ifelse(pw > 0, pw, 1)))
+    ## paranoid version above should not be needed with lbp, and version below makes deriv easier.
+    k <- sum(pdf * log(pdf)) - sum(pc * log(pc)) - sum(pw * log(pw))
+    ## obf <- EU - lambda * k
+    ## two lines below apply a penalty for pdf values near zero.  This prevents the search from wandering over large
+    ## changes in large negative values of log p's, and may speed convergence.
     ##---------------------------------
+    ## zerop <- pdf[ (pdf < lbp)]          #sum(pdf) is one by construction, so don't penalize its getting close to one.
+    zerop <- pc[ pc < lbp ]
+    if (sum(pc) > 1 - lbp ) zerop <- c(zerop, 1-sum(pc))
+    zerodc <- dc[dc < lbp]
+    if (sum(dc) > 1 - lbp ) zerodc <- c(zerodc, 1-sum(dc))
+    zerodc <- to.tensor(zerodc, c(zc=length(zerodc)))
+    ## zerodc <- dc[dc < 0]
+    ## if(sum(dc) >1) zerodc <- c(zerodc, 1-sum(dc))
+    ## obf <- EU  - PNLTY * (sum((lbp / zerop - 1)^2 ) + sum((lbp / zerodc -1)^2) + 100 * sum(abs(rep(1,nc) %*% pdf - pw)) + 10 * max(k-kappa,0)^2)
+    ##obf <- EU  - PNLTY * (sum((lbp / zerop - 1)^2 ) + sum((lbp / zerodc -1)^2)  + 10 * max(k-kappa,0)^2)
+    obf <- EU  - PNLTY * (sum((lbp / zerop - 1)^2 ) + sum(zerodc^2)  + 10 * max(k-kappa,0)^2)
     return(list(obf=obf, EU=EU, cc=cc, pc=pc, pdf=pdf, info=k/log(2)))
   }
-}
-
-##!! below is just above with deriv sections removed.
-screpDO <-   function(dc=rep(.1,9), pc=rep(.111,8), alpha=1) {
-  ## computes objective function only, including the penalty defining the constraints.
-  ## dc:    determines the points in crange that form the support of the c distribution.  The entries are all positive and
-  ##        sum to one.  Support point i is at (sum_1^i dc_i) * (crange[2] - crange[1]) + crange[1].
-  ## pc:    Marginal probabilities on the points in c's support, with last one omitted ( = 1 - sum of others).
-  ## alpha: 1/(Lagrange multiplier on info constraint).  This is treated as a free parameter, so that the FOC's
-  ##        let us write the joint density as pc %*% exp(alpha*U) %*% h(w) for a pdf pc and a weighting function h.
-  require("tensorA")
-  if ((sum(dc) > 1) || any( dc < 0 ) ) {
-    message("bad dc vector")
-    return(list(obf=-1e100))
-  }
-  if ((sum(pc) > 1) || any (pc < 0 ) ) {
-    message("bad pc vector")
-    return(list(obf=-1e100))
-  }
-  cc <- to.tensor(cumsum(dc),dims=c(ic=nc))
-  pc <- to.tensor(c(pc, 1 - sum(pc), dims=c(ic=nc)))
-  cc <- cc * (crange[2] - crange[1]) + crange[1]
-  ww <- to.tensor(ww,dims=c(jw=nw))
-  pw <- to.tensor(pw,dim=c(jw=nw))
-  ## cltw <- outer(cc, ww, function(a,b){a<b-.Machine$double.eps*2} )
-  Umat <- to.tensor(outer(cc, ww, FUN=U), dims=c(dim(cc), dim(ww)), what=c(1,2))
-  ## deriv---------------
-  ##     dUmatdcc <- to.tensor(0,c(ic=nc, iw=nw, idc=nc))
-  ##     for (i in 1:nc) dUmatdcc[i, , i] <- DxU(cc[i],ww)
-  ## /deriv---------------
-  eaU <- exp(alpha * Umat)
-  ## deriv----------------
-  ##     deaUdcc <- to.tensor(array(eaU, c(ic=nc, iw=nw, idc=nc)), dims=c(ic=nc, iw=nw, idc=nc), what=1:3)
-  ##     deaUdcc <- deaUdcc * dUmatdcc * alpha
-  ##     deaUdalpha <- eaU * Umat
-  ## /deriv---------------
-  peaU <- mul.tensor(pc, eaU, by="ic")
-  ## deriv----------------
-  ##     dpeaUdpc <- to.tensor(0, c(ic=nc, iw=nw, idc=nc))
-  ##     for (i in 1:nc) dpeaUdpc[i, , i] <- eaU[i, ]
-  ##     dpeaUdcc <- pc * deaUdcc
-  ##     dpeaUdalpha <- peaU * Umat
-  ## /deriv---------------
-  pwhat <- margin.tensor(peaU, "ic")
-  ## deriv----------------
-  ##     dpwhatdcc <- margin.tensor(dpeaUdcc, "ic")
-  ##     dpwhatdpc <- margin.tensor(dpeaUdpc, "ic")
-  ##     dpwhatdalpha <-  margin.tensor(dpeaUdalpha,"ic")
-  ## /deriv---------------
-  h <- pw/pwhat
-  ## deriv---------------
-  ##     dhdpwhat <-  -h/pwhat
-  ##     dhdcc <- dhdpwhat * dpwhatdcc
-  ##     dhdpc <- dhdpwhat * dpwhatdpc
-  ##     dhdalpha <- dhdpwhat * dpwhatdalpha
-  ## /deriv--------------
-  pdf <- mul.tensor(h, NULL, peaU, NULL, by="iw")
-  ## deriv--------------
-  ##     dpdfdh <- to.tensor(0, c(ic=nc, iw=nw, idw=nw))
-  ##     for (j in 1:nw) dpdfdh[ ,j, j] <- peaU[,j]
-  ##     dpdfdpc <- to.tensor(0,c(ic=nc,iw=nw,idc=nc))
-  ##     eaUh <- mul.tensor(h, NULL, eaU, NULL, by="iw")
-  ##     for (i in 1:nc) dpdfdpc[i, , i] <- eaUh[i, ]
-  ##     dpdfdpc <- dpdfdpc + mul.tensor(dpdfdh, "idw", dhdpc, "iw")
-  ##     dpdfdcc <- to.tensor(0,c(ic=nc, iw=nw, idc=nc))
-  ##     dpdfdalpha <- mul.tensor(dhdalpha, NULL, peaU, NULL, by="iw") + mul.tensor(h, NULL, dpeaUdalpha, NULL, by="iw")
-                                        # #t(c(dhdalpha) * t(peaU)) + t(h * t(dpeaUdalpha)) 
-  ## /deriv---------------
-  h <- margin.tensor(pdf, ic) #rep(1,nc) %*% pdf #Is this redundant?  Looks like should have h == 1, so pdf unchanged.
-  h <- pw / h
-  pdf <- mul.tensor(h, NULL, pdf, NULL, by="iw") #t(c(h) * t(pdf ))
-  EU <- sum(pdf * Umat)
-  k <- sum(pdf * log(ifelse(pdf>0,pdf,1))) - sum(pc * log(ifelse(pc>0,pc,1))) - sum(pw * log(ifelse(pw > 0, pw, 1)))
-  ## obf <- EU - lambda * k
-  ## two lines below apply a penalty for pdf values near zero.  This prevents the search from wandering over large
-  ## changes in large negative values of log p's, and may speed convergence.
-  ##---------------------------------
-  zerop <- pdf[ cltw & (pdf < lbp)]
-  ## zerodc <- dc[dc < lbp]
-  ## if (sum(dc) > 1 - lbp ) zerodc <- c(zerodc, 1-sum(dc))
-  zerodc <- dc[dc<0]
-  if(sum(dc) >1) zerodc <- c(zerodc, 1-sum(dc))
-  ## obf <- EU  - PNLTY * (sum((lbp / zerop - 1)^2 ) + sum((lbp / zerodc -1)^2) + 100 * sum(abs(rep(1,nc) %*% pdf - pw)) + 10 * max(k-kappa,0)^2)
-  ##obf <- EU  - PNLTY * (sum((lbp / zerop - 1)^2 ) + sum((lbp / zerodc -1)^2)  + 10 * max(k-kappa,0)^2)
-  obf <- EU  - PNLTY * (sum((lbp / zerop - 1)^2 ) + sum(zerodc^2)  + 10 * max(k-kappa,0)^2)
-  ##---------------------------------
-  return(list(obf=obf, EU=EU, cc=cc, pc=pc, pdf=pdf, info=k/log(2)))
+  return(list(dobf=screpDC, obf=screpDCO))
 }

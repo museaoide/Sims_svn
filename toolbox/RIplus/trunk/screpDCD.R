a9fe37e7ebfd160a7576b1308609c997af29ab65
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2009-01-14 16:45:09 +0000

    work on screpDCD while in SF

--- toolbox/RIplus/trunk/screpDCD.R
+++ toolbox/RIplus/trunk/screpDCD.R
@@ -65,8 +65,8 @@ setUpScrep <- function(crange=c(0.,0.5), pw, ww,
     pwhat <- margin.tensor(peaU, "ic")
     ## deriv----------------
     ## the first two of these, below are compacted:  cc[i] and pc[i] affect only column i of peaU
-    dpwhatdcc <- dpeaUdcc # compacted
-    dpwhatdpc <- dpeaUdpc #compacted
+    dpwhatdcc <- dpeaUdcc               # compacted
+    dpwhatdpc <- dpeaUdpc               #compacted
     dpwhatdalpha <-  margin.tensor(dpeaUdalpha,"ic")
     ## /deriv---------------
     h <- pw/pwhat                       
@@ -74,27 +74,28 @@ setUpScrep <- function(crange=c(0.,0.5), pw, ww,
     dhdpwhat <-  -h/pwhat               #compacted.
     dhdcc <- diagmul.tensor(dhdpwhat, "iw", dpwhatdcc)
     dhdpc <- diagmul.tensor(dhdpwhat, "iw", dpwhatdpc)
-    dhdalpha <- mul.tensor(dhdpwhat, "iw", dpwhatdalpha)
+    dhdalpha <- diagmul.tensor(dhdpwhat, "iw", dpwhatdalpha)
     ## /deriv--------------
     pdf <- diagmul.tensor(h, "iw", peaU)
     ## deriv--------------
-    dpdfdh <- peaU                      #compacted
+    dpdfdh <- peaU  #compacted
     ## dpdfdh <- to.tensor(0, c(ic=nc, iw=nw, idw=nw))
     ## for (j in 1:nw) dpdfdh[ic=1:nc, iw=j, idw=j] <- peaU[ic=1:nc, iw=j]
     ##!! forgot the need to recognize that only diagonal elements are non-zero and messed up some lines below.
     ## dpdfdpc <- to.tensor(0,c(ic=nc,iw=nw,idc=nc))
     ## eaUh <- diagmul.tensor(h, "iw", eaU)
-    dpdfdpc <- diagmul.tensor(h, "iw", dpeaUdpc) #compacted
-    dpdfdpc <- dpdfdpc + diagmul.tensor(dhdpc, "iw", peaU) #compacted
+    dpdfdpc <- diagmul.tensor(h, "iw", dpeaUdpc)
+    dpdfdpc <- diag.tensor(dpdfdpc,  by="iw", mark="d")[[icd=~idp]] + diagmul.tensor(dhdpc[[ic=~idp]], "iw", peaU) 
     ## for (i in 1:nc) dpdfdpc[ic=i,iw=1:nw , idc=1:nc] <- eaUh[ic=i, iw=1:nw]
     ## dpdfdpc <- dpdfdpc + mul.tensor(dpdfdh, "idw", dhdpc, "iw")
-    dpdfdcc <- diagmul.tensor(dhdcc, "iw", peaU) + diagmul.tensor(h, "iw", dpeaUdcc)
+    dpdfdcc <- diagmul.tensor(h, "iw", dpeaUdcc)
+    dpdfdcc <- diag.tensor(dpdfdcc, by="iw", mark="d")[[icd=~idc]] + diagmul.tensor(dhdcc[[ic=~idc]], "iw", peaU)
     dpdfdalpha <- diagmul.tensor(dhdalpha,"iw", peaU) + diagmul.tensor(h, "iw", dpeaUdalpha)
                                         # #t(c(dhdalpha) * t(peaU)) + t(h * t(dpeaUdalpha)) 
     ## /deriv---------------
     h0 <- margin.tensor(pdf, "ic")
     ## deriv----------------------
-    dh0dcc <- margin.tensor(dpdfdcc,"ic")
+    dh0dcc <- margin.tensor(dpdfdcc, "ic")
     dh0dpc <- margin.tensor(dpdfdpc, "ic")
     dh0dalpha <- margin.tensor(dpdfdalpha, "ic")
     ## /deriv----------------------
@@ -112,52 +113,67 @@ setUpScrep <- function(crange=c(0.,0.5), pw, ww,
     ## /deriv--------------
     EU <- sum(pdf * Umat)
     ## deriv-----------------
-    dEUdcc <- margin(diagmul.tensor(dpdfdcc, names(Umat), Umat), names(Umat))
-    dEUdpc <- margin(diagmul.tensor(dpdfdpc, names(Umat), Umat), names(Umat))
-    dEUdalpha <- margin(diagmul.tensor(dpdfdalpha, names(Umat), Umat), names(Umat))
+    dEUdcc <- margin.tensor(diagmul.tensor(dpdfdcc, names(Umat), Umat), names(Umat))
+    dEUdpc <- margin.tensor(diagmul.tensor(dpdfdpc, names(Umat), Umat), names(Umat))
+    dEUdalpha <- margin.tensor(diagmul.tensor(dpdfdalpha, names(Umat), Umat), names(Umat)) #== sum(dpdfdalpha * Umat)?
     ## /deriv------------------
     ## k <- sum(pdf * log(ifelse(pdf>0,pdf,1))) - sum(pc * log(ifelse(pc>0,pc,1))) - sum(pw * log(ifelse(pw > 0, pw, 1)))
     ## paranoid version above should not be needed with lbp, and version below makes deriv easier.
     k <- sum(pdf * log(pdf)) - sum(pc * log(pc)) - sum(pw * log(pw))
     ## deriv-------------------------
-    dkdcc <- margin(diagmul.tensor(dpdfdcc, names(pdf), log(pdf) ) + dpdfdcc, names(pdf))
-    dkdpc <- margin(diagmul.tensor(dpdfdpc, names(pdf), log(pdf) ) + dpdfdpc, names(pdf)) - log(pc)[[ic=~idp]] - one.tensor(c(idp=nc))
-    dkdalpha <- margin(diagmul.tensor(dpdfdalpha, names(pdf), log(pdf) ) + dpdfdalpha, names(pdf))
+    dkdcc <- margin.tensor(diagmul.tensor(dpdfdcc, names(pdf), log(pdf) ) + dpdfdcc, names(pdf))
+    dkdpc <- margin.tensor(diagmul.tensor(dpdfdpc, names(pdf), log(pdf) ) + dpdfdpc, names(pdf))
+    - log(pc)[[ic=~idp]] - one.tensor(c(idp=nc))
+    dkdalpha <- margin.tensor(diagmul.tensor(dpdfdalpha, names(pdf), log(pdf) ) + dpdfdalpha, names(pdf))
     ## /deriv--------------------
     ## obf <- EU - lambda * k
     ## two lines below apply a penalty for pdf values near zero.  This prevents the search from wandering over large
     ## changes in large negative values of log p's, and may speed convergence.
     ##---------------------------------
     ## zerop <- pdf[ (pdf < lbp)]          #sum(pdf) is one by construction, so don't penalize its getting close to one.
-    zerop <- pc[ pc < lbp ]
-    if (sum(pc) > 1 - lbp ) zerop <- c(zerop, 1-sum(pc))
-    zerodc <- dc[dc < lbp]
-    if (sum(dc) > 1 - lbp ) zerodc <- c(zerodc, 1-sum(dc))
-    zerodc <- to.tensor(zerodc, c(zc=length(zerodc)))
-    ## deriv------------------------
-    zerop <- to.tensor(zerop, c(izp=length(zerop)))
-    zerodc <- to.tensor(zerodc, c(izc=length(zerodc)))
-    ddcdcc <- toeplitz(nc:1)
-    ddcdcc[lower.tri(ddcdcc)] <- 0
-    ddcdcc <- solve(ddcdcc)
-    ddcdcc <- as.tensor(ddcdcc, dims=c(iddc=nc,idc=nc))
-    dzeropdp <- delta.tensor(c(idp=nc))$izp.idp
-    dzeropdp <- dzeropdp[ izp= (pc < lbp), idp=1:nc, drop=FALSE]
-    dzerocdcc <- delta.tensor(c(iddc=nc))$izc.iddc
-    dzerocdcc <- dzerocdcc[ izc= (dc < lbp), iddc=1:nc, drop=FALSE]
-    dzerocdcc <- mul.tensor(dzerocdcc, "iddc", ddcdcc, "iddc")
+    if (!any(pc < lbp)) {
+      zpPnlty <- 0
+      dzpPnlty <- 0
+    } else {
+      zerop <- pc[ pc < lbp ]
+      zerop <- to.tensor(zerop, c(izp=length(zerop)))
+      if (sum(pc) > 1 - lbp ) zerop <- c(zerop, 1-sum(pc))
+      dzeropdp <- delta.tensor(c(idp=nc))$izp.idp
+      dzeropdp <- dzeropdp[ izp= (pc < lbp), idp=1:nc, drop=FALSE]
+      zpPnlty <- sum((lbp / zerop - 1)^2 ) 
+      dzpPnlty <- mul.tensor(2 * (lbp / zerop -1) * (-lbp / zerop^2), "izp", dzeropdp,  "izp")
+    }
+    if(!any(dc < lbp)) {
+      zcPnlty <- 0
+      dzcPnlty <- 0
+    } else {
+      zerodc <- dc[dc < lbp]
+      if (sum(dc) > 1 - lbp ) zerodc <- c(zerodc, 1-sum(dc))
+      zerodc <- to.tensor(zerodc, c(zc=length(zerodc)))
+      ## deriv------------------------
+      zerodc <- to.tensor(zerodc, c(izc=length(zerodc)))
+      ddcdcc <- toeplitz(nc:1)
+      ddcdcc[lower.tri(ddcdcc)] <- 0
+      ddcdcc <- solve(ddcdcc)
+      ddcdcc <- as.tensor(ddcdcc, dims=c(iddc=nc,idc=nc))
+      dzerocdcc <- delta.tensor(c(iddc=nc))$izc.iddc
+      dzerocdcc <- dzerocdcc[ izc= (dc < lbp), iddc=1:nc, drop=FALSE]
+      dzerocdcc <- mul.tensor(dzerocdcc, "iddc", ddcdcc, "iddc")
+      dzcPnlty <- mul.tensor(2 * (lbp / zerop -1) * (-lbp / zerop^2), "izp", dzerocdcc,  "izp")
+      zcPnlty <- sum((lbp / zeroc -1)^2)
+    }
     ## /deriv------------------------
     ## zerodc <- dc[dc < 0]
     ## if(sum(dc) >1) zerodc <- c(zerodc, 1-sum(dc))
     ## obf <- EU  - PNLTY * (sum((lbp / zerop - 1)^2 ) + sum((lbp / zerodc -1)^2) + 100 * sum(abs(rep(1,nc) %*% pdf - pw)) + 10 * max(k-kappa,0)^2)
     ##obf <- EU  - PNLTY * (sum((lbp / zerop - 1)^2 ) + sum((lbp / zerodc -1)^2)  + 10 * max(k-kappa,0)^2)
-    obf <- EU  - PNLTY * (sum((lbp / zerop - 1)^2 ) + sum(zerodc^2)  + 10 * max(k-kappa,0)^2)
+    obf <- EU  - PNLTY * (zpPnlty + zcPnlty  + 10 * max(k-kappa,0)^2)
     ## deriv-------------------------------
-    dobfdcc <- dEUdcc - PNLTY * (mul.tensor(2 * (lbp / zeroc -1) * (-lbp / zeroc^2), "izc", dzerocdcc, "izc") + 10 * (k > kappa) * 2 * (k - kappa) * dkdcc)
-    dobfdpc <- dEUdpc - PNLTY * (mul.tensor(2 * (lbp / zerop -1) * (-lbp / zerop^2), "izp", dzeropdp,  "izp") + 10 * (k > kappa) * 2  * (k - kappa) * dkdpc)
-    dobfdalpha <- dEUdalpha - PNLTY * (10 * (k > kappa) * 2 * (k - kappa) * dkdalplha)
+    dobfdcc <- dEUdcc - PNLTY * (dzcPnlty + 10 * (k > kappa) * 2 * (k - kappa) * dkdcc)
+    dobfdpc <- dEUdpc - PNLTY * (dzpPnlty + 10 * (k > kappa) * 2  * (k - kappa) * dkdpc)
+    dobfdalpha <- dEUdalpha - PNLTY * (10 * (k > kappa) * 2 * (k - kappa) * dkdalpha)
     ## /deriv---------------------------------
-    return(list(obf=obf, EU=EU, cc=cc, pc=pc, pdf=pdf, info=k/log(2), dobfdcc, dobfdpc, dobfdalpha))
+    return(list(obf=obf, EU=EU, cc=cc, pc=pc, pdf=pdf, info=k/log(2), dobfdcc=dobfdcc, dobfdpc=dobfdpc, dobfdalpha=dobfdalpha))
   }
   screpDCO <- function(dc=rep(.1,9), pc=rep(.111,8), alpha=1) {
     ## computes objective function, including the penalty defining the constraints.

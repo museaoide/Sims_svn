947ff25156d833c01cdeb0945886dbc60c6784f9
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2009-12-23 14:25:29 +0000

    Latest DiscPObj

--- toolbox/RIplus/trunk/DiscreteX/DiscPObj.R
+++ toolbox/RIplus/trunk/DiscreteX/DiscPObj.R
@@ -1,59 +1,89 @@
-DiscPObj <- function(param, gx, y, U, alph) {
+DiscPObj <- function(param, gy, y, U, alph) {
+  ## param:  First nx-1 are marginal probabilities on x points,
+  ##         remainder are the positions of the discrete x points.
+  ## gy:     The marginal probabilities on y
+  ## y:      The discrete y points
+  ## U:      A function of x and y defining utility
+  ## alph:   One over the utility cost of information
+  ## obj:    The objective function value.
   ## "p" and "x" index the variables wrt which we are taking derivs
   ## "weight" indexes the nx rows of the joint density
   ## "y" indexes the ny columns of the joint density
-  require(tensorA)
+  require("tensor")
   nx <- (length(param)+1)/2
+  ny <- length(gy)
   p <- param[1:(nx - 1)]
-  p <- to.tensor(c(p, 1 - sum(p)), c(1, "weight"))
-  x <- to.tensor(param[nx:(2 * nx - 1)]. c(1, "weight"))
-  ny <- length(gx)
+  p <- matrix(c(p, 1 - sum(p)), 1,nx, dimnames=list(NULL, p=NULL))
+  x <- matrix(param[nx:(2 * nx - 1)], 1, nx, dimnames=list(NULL, x=NULL))
   if ( any(p < 0) ) return(1e20)
   mm <- function(z) { matrix(z, nx, ny, byrow=TRUE) }
   sy <- function(z) {apply( z, MAR=1, FUN=sum)}
-  Umat <- to.tensor(outer(x, y, FUN=U),c(weight=nx, y=ny))
+  Umat <- outer(c(x), c(y), FUN=U)
   ## peaU <- p %*% exp(alph * Umat)
-  eaU <- exp(alph * Umat),
-  DPpeaU <- eaU[[weight=~p]]            #renames one dim
-  peaU <- p %*% eaU                     #warning:  this, like some others below, is really only the
-                                        #  diagonal of DPpeaU, truly indexed
-                                        #  by both p and weight
-  DXU <- to.tensor(attr(Umat,"gradient"), c(x=nx, y=ny))
+  eaU <- exp(alph * Umat)
+  dimnames(eaU) <- list(weight=NULL, y=NULL)
+  DPpeaU <- eaU            
+  names(dimnames(DPpeaU))[1] <- "p"
+  peaU <- tensor(p, eaU, 2,1)
+  DXU <- matrix(attr(Umat,"gradient"), nx, ny, dimnames=list(x=NULL, y=NULL))
   ##assumes U fcn written to return gradient attribute,
   ##e.g. via deriv().
-  DXpeaU <- mm(p %*% (alph * eaU)) * alph * DXU
-  h <-  gx / peaU
-  DPh <- -DPpeaU * mm(c(gx / peaU^2))
-  DXh <- -DXpeaU * mm(c(gx / peaU^2))
+  DXpeaU <- mm(tensor(p, alph * eaU, 2, 1)) * DXU
+  h <-  gy / peaU
+  DPh <- -DPpeaU * mm(c(gy / peaU^2))
+  DXh <- -DXpeaU * mm(c(gy / peaU^2))
   eaUh <- eaU * mm(c(h))
   f <- c(p) * eaUh
-  DPf <- eaUh +  p * eaU * DPh
-  DXf <- c(p) * eaU * DXh + c(p) * (alph * DXU * eaUh)
+  DPf <- array(0, c(nx,nx,ny), dimnames=list(p=NULL, weight=NULL, y=NULL))
+  for (ip in 1:nx){
+    for (iw in 1:nx) {
+      DPf[ip, iw, ] <- p[iw]*eaU[iw, ] * DPh[ip, ] 
+    }
+    DPf[ip,ip, ] <- DPf[ip,ip, ] + eaUh[ip, ]
+  }
+  DXf <- array(0,c(nx,nx,ny), dimnames=list(x=NULL, weight=NULL, y=NULL))
+  for (ix in 1:nx) {
+    for (iw in 1:nx) {
+      DXf[ix,iw, ] <- p[iw] * eaU[iw, ] * DXh[ix, ]
+    }
+    DXf[ix, ix, ] <- DXf[ix, ix, ] + p[ix] * eaUh[ix, ] * alph * DXU[ix, ] * h
+  }
+  DPobj <- tensor(DPf, Umat, c(2,3), c(1,2))
+  DXobj <- tensor(DXf, Umat, c(2,3), c(1,2)) + sy(f * DXU * alph)
+  ## Above are derivatives of the expected utility part.  On to the info cost part.
   pnew <- sy(f)
-  DPpnew <- sy(DPf)
-  DXpnew <- sy(DXf)
+  DPpnew <- tensor(DPf, rep(1, ny), 3, 1)
+  DXpnew <- tensor(DXf, rep(1, ny), 3, 1)
   ipplus <- p > 0
   fplus <- f[ipplus, ]
   pplus <- pnew[ipplus]
-  roweight <- to.tensor(sy(eaUh), c(weight=nx,1))
+  roweight <- sy(eaUh)
   ygivenx <- c(1/roweight) * eaUh
-  DProweight <- to.tensor(eaU %*% t(DPh), c(weight=nx, p=nx)) #nx x nx matrix
-  DXroweight <- to.tensor(sy( eaU * DXh) + (eaU * alph * DXU) %*% h, c(weight=nx, x=nx)) 
+  DProweight <- tensor(DPh, eaU, 2, 2)  #p=nx x w=nx matrix
+  DXroweight <- diag(sy(eaU * alph * DXU)) + tensor(DXh, eaU, 2, 2)
+  DPygivenx <- array(0,c(nx,nx,ny))
+  DXygivenx <- array(0,c(nx,nx,ny))
   ## result of line below is weight=nx by p=nx by y=ny tensor.  Weight indexes rows of ygivenx, p indexes what deriv's are wrt.
-  DPygivenx <- -mul.tensor(DProweight,NULL , 1/roweight^2, NULL, by="weight")
-               + mul.tensor(to.tensor( (1/roweight) * eaU , c(weight=nx, y=ny)), NULL, to.tensor(DPh, c(p=nx, y=ny), by="y"))
-                     
-  DXygivenx <- -mul.tensor(DXroweight, NULL, 1/roweight^2, NULL, by="weight")
-               + mul.tensor(to.tensor( (1/rowweight) * eaU, c(weight=nx, y=ny)), NULL, to.tensor(DXh, c(p=nx, y=ny), by="y"))
-    obj <- sum(f  * Umat ) - (1/alph) * pplus %*% sy(log(ygivenx[ipplus]) * ygivenx[ipplus])
-  DPobj <- vector("numeric", nx)
-  DXobj <- DPobj
-  DPobj[ipplus] <- sy(DPf * Umat)[ipplus] - (1/alph) * 
-  DXobj[ipplus] <- apply(DXU * f, MAR=1, FUN=sum)[ipplus]
-  + apply(Umat * DXf, MAR=1, FUN=sum)[ipplus] -(1/alph) * apply(log(ygivenx[ipplus]) * DXf[ipplus, ], FUN=sum, MAR=1)
+  for (iw in 1:nx) {
+    DPygivenx[ , iw, ] <- DProweight[ , iw] %o% (-(1/roweight[iw]^2) * eaUh[iw, ])
+  }
+  for (iy in 1:ny) {
+    DPygivenx[ , , iy] <- DPh[ , iy] %o% (c(1/roweight) * eaU[ , iy])
+  }
+  for (iw in 1:nx) {
+    DXygivenx[ , iw, ] <- DXroweight[ , iw] %o% ((-1/roweight[iw]^2) * eaUh[iw, ])
+    DXygivenx[iw, iw, ] <- DXygivenx[iw,iw,] + (1/roweight[iw]^2) * eaUh[iw, ] * DXU[iw, ] *alph 
+  }
+  for (iy in 1:ny) {
+    DXygivenx[ , , iy] <- DXygivenx[ , , iy] + DXh[ , iy] %o% (c(1/roweight) * eaU[ , iy])
+  }                                                           
+  obj <- sum(f  * Umat ) - (1/alph) * pnew %*% sy(log(ygivenx) * ygivenx)
+  DPobj <- c(DPobj) - c((1/alph) * DPpnew %*% sy(log(ygivenx) * ygivenx)) + c(tensor(DPygivenx, c(pnew) * (1 + log(ygivenx)), 2:3, 1:2))
+  DXobj <- c(DXobj) - c((1/alph) * DXpnew %*% sy(log(ygivenx) * ygivenx)) + c(tensor(DPygivenx, c(pnew) * (1 + log(ygivenx)), 2:3, 1:2))
   obj <- -obj                           #as input to minimizer
-  attr(obj, "gradient") <- -c(cbind(diag(nx - 1), rep(-1, nx - 1)) %*% DPobj, DXobj)
-                                        # browser()
-                                        #attr(obj, "pnew") <- pnew             #allows checking that pnew = p (as it should at optimum)
+  attr(obj, "gradient") <- -c(cbind(diag(nx - 1), rep(-1, nx - 1)) %*% DPobj, DXobj) #recongizing that there are only nx-1 p parameters, with
+                                        # n'th being 1 minus the rest.
+  ## browser()
+  ##attr(obj, "pnew") <- pnew             #allows checking that pnew = p (as it should at optimum)
   return(obj)
 }

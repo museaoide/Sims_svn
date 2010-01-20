9f971de7ec7a0fcca00f3ac3b10be2c4e9435a23
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2010-01-20 05:05:52 +0000

    Update of Discrete Prob RIPlus code

--- toolbox/RIplus/trunk/DiscreteX/DiscPObj.R
+++ toolbox/RIplus/trunk/DiscreteX/DiscPObj.R
@@ -9,6 +9,9 @@ DiscPObj <- function(param, gy, y, U, alph) {
   ## "p" and "x" index the variables wrt which we are taking derivs
   ## "weight" indexes the nx rows of the joint density
   ## "y" indexes the ny columns of the joint density
+  ##------------------------
+  ## 18.1.10, verified results vs. numerical gradients on nx=4 problem
+  ##
   require("tensor")
   nx <- (length(param)+1)/2
   ny <- length(gy)
@@ -28,7 +31,8 @@ DiscPObj <- function(param, gy, y, U, alph) {
   DXU <- matrix(attr(Umat,"gradient"), nx, ny, dimnames=list(x=NULL, y=NULL))
   ##assumes U fcn written to return gradient attribute,
   ##e.g. via deriv().
-  DXpeaU <- mm(tensor(p, alph * eaU, 2, 1)) * DXU
+  ## DXpeaU <- mm(tensor(p, alph * eaU, 2, 1)) * DXU
+  DXpeaU <- c(p) * (alph * eaU * DXU)
   h <-  gy / peaU
   DPh <- -DPpeaU * mm(c(gy / peaU^2))
   DXh <- -DXpeaU * mm(c(gy / peaU^2))
@@ -46,7 +50,8 @@ DiscPObj <- function(param, gy, y, U, alph) {
     for (iw in 1:nx) {
       DXf[ix,iw, ] <- p[iw] * eaU[iw, ] * DXh[ix, ]
     }
-    DXf[ix, ix, ] <- DXf[ix, ix, ] + p[ix] * eaUh[ix, ] * alph * DXU[ix, ] * h
+    ## DXf[ix, ix, ] <- DXf[ix, ix, ] + p[ix] * eaUh[ix, ] * alph * DXU[ix, ] * h
+    DXf[ix, ix, ] <- DXf[ix, ix, ] + p[ix] * eaUh[ix, ] * alph * DXU[ix, ] #the h on the end redundant (?)
   }
   DPobj <- tensor(DPf, Umat, c(2,3), c(1,2))
   DXobj <- tensor(DXf, Umat, c(2,3), c(1,2)) + sy(f * DXU * alph)
@@ -60,26 +65,26 @@ DiscPObj <- function(param, gy, y, U, alph) {
   roweight <- sy(eaUh)
   ygivenx <- c(1/roweight) * eaUh
   DProweight <- tensor(DPh, eaU, 2, 2)  #p=nx x w=nx matrix
-  DXroweight <- diag(sy(eaU * alph * DXU)) + tensor(DXh, eaU, 2, 2)
+  DXroweight <- diag(sy(eaUh * alph * DXU)) + tensor(DXh, eaU, 2, 2)
   DPygivenx <- array(0,c(nx,nx,ny))
   DXygivenx <- array(0,c(nx,nx,ny))
-  ## result of line below is weight=nx by p=nx by y=ny tensor.  Weight indexes rows of ygivenx, p indexes what deriv's are wrt.
+  ## result of line below is p=nx by weight=nx by y=ny tensor.  Weight indexes rows of ygivenx, p indexes what deriv's are wrt.
   for (iw in 1:nx) {
     DPygivenx[ , iw, ] <- DProweight[ , iw] %o% (-(1/roweight[iw]^2) * eaUh[iw, ])
   }
   for (iy in 1:ny) {
-    DPygivenx[ , , iy] <- DPh[ , iy] %o% (c(1/roweight) * eaU[ , iy])
+    DPygivenx[ , , iy] <- DPygivenx[ , , iy] + DPh[ , iy] %o% (c(1/roweight) * eaU[ , iy])
   }
   for (iw in 1:nx) {
     DXygivenx[ , iw, ] <- DXroweight[ , iw] %o% ((-1/roweight[iw]^2) * eaUh[iw, ])
-    DXygivenx[iw, iw, ] <- DXygivenx[iw,iw,] + (1/roweight[iw]^2) * eaUh[iw, ] * DXU[iw, ] *alph 
+    DXygivenx[iw, iw, ] <- DXygivenx[iw,iw,] + (1/roweight[iw]) * eaUh[iw, ] * DXU[iw, ] *alph 
   }
   for (iy in 1:ny) {
     DXygivenx[ , , iy] <- DXygivenx[ , , iy] + DXh[ , iy] %o% (c(1/roweight) * eaU[ , iy])
   }                                                           
-  obj <- sum(f  * Umat ) - (1/alph) * pnew %*% sy(log(ygivenx) * ygivenx)
-  DPobj <- c(DPobj) - c((1/alph) * DPpnew %*% sy(log(ygivenx) * ygivenx)) + c(tensor(DPygivenx, c(pnew) * (1 + log(ygivenx)), 2:3, 1:2))
-  DXobj <- c(DXobj) - c((1/alph) * DXpnew %*% sy(log(ygivenx) * ygivenx)) + c(tensor(DPygivenx, c(pnew) * (1 + log(ygivenx)), 2:3, 1:2))
+  obj <- sum(f  * Umat ) - (1/alph) * pnew %*% sy(log(ygivenx) * ygivenx) # + (1/alpha) * sum(gy * log(gy)) (doesn't change with param)
+  DPobj <- c(DPobj) - c((1/alph) * (DPpnew %*% sy(log(ygivenx) * ygivenx) + c(tensor(DPygivenx, c(pnew) * (1 + log(ygivenx)), 2:3, 1:2))))
+  DXobj <- c(DXobj) - c((1/alph) * (DXpnew %*% sy(log(ygivenx) * ygivenx) + c(tensor(DXygivenx, c(pnew) * (1 + log(ygivenx)), 2:3, 1:2))))
   obj <- -obj                           #as input to minimizer
   attr(obj, "gradient") <- -c(cbind(diag(nx - 1), rep(-1, nx - 1)) %*% DPobj, DXobj) #recongizing that there are only nx-1 p parameters, with
                                         # n'th being 1 minus the rest.

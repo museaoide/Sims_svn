b81bce003a4a8f90ad9d28f1a4e1fbc31cd0bca4
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2010-07-08 03:51:43 +0000

    non-substantive change

--- toolbox/VARtools/trunk/pchol.R
+++ toolbox/VARtools/trunk/pchol.R
@@ -1,4 +1,4 @@
 pchol <- function(sig, porder) {
   invporder <- match(1:length(porder), porder)
-  chol(sig[porder, porder])[invporder, invporder]
+  return(chol(sig[porder, porder])[invporder, invporder])
 }

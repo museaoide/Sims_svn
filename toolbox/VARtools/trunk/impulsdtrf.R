346f115941b304f61e77831fdbc25de522f76b90
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2010-07-08 03:50:32 +0000

    Allow non-square smat, integrate with rfvar3 output structure

--- toolbox/VARtools/trunk/impulsdtrf.R
+++ toolbox/VARtools/trunk/impulsdtrf.R
@@ -1,35 +1,38 @@
-impulsdtrf <- function(B,smat,nstep)
-### By:             As emerges from rfvar, neqn x nvar x lags array of rf VAR coefficients.
-### smat:           nshock x nvar matrix of initial shock vectors.  To produce "orthogonalized
-###                 impulse responses" it should have the property that crossprod(t(smat))=sigma,
-###                 where sigma is the Var(u(t)) matrix and u(t) is the rf residual vector.  One
-###                 way to get such a smat is to set smat=t(chol(sigma)).  To get the smat
-###                 corresponding to a different ordering, use
-###                 smat = t(chol(Sigma[ndx, ndx])[ndxi, ndxi] ), where ndx is a permutation vector and
-###                 ndxi is its inverse (e.g. c(3,2,1) and c(3,2,1), or c(2,3,1) and c(3,1,2)).
-###                 To get impulse responses for a structural VAR in the form A(L)y=eps, with
-###                 Var(eps)=I, use B(L)=-A_0^(-1)A_+(L) (where A_+ is the coefficients on strictly
-###                 positive powers of L in A), smat=A_0^(-1).
-###                 In general, though, it is not required that smat be invertible.
+impulsdtrf <- function(vout, smat=NULL, nstep=40, order=NULL)
+### vout:           output structure from rfvar3
+### smat:           if NULL, the impulse responses will be for a cholesky decomp with variables
+###                 ordered as in the input to rfvar3.  More generally, can be any set of initia
+###                 values for the shocks.  To keep the shocks orthogonal and scaled properly,
+###                 smat should be such that t(smat) %*% smat == crossprod(vout$u)/dim(u)[1].
+###                 However, the routine works with singular smat or with smat's column dimension
+###                 less than its row dimension.
+### order:          To get a cholesky decomp with a different ordering, set order to an integer
+###                 vector giving the desired ordering.  Ignored if smat non-NULL.  
 ### response:       nvar x nshocks x nstep array of impulse responses.
 ###
 ### Code written by Christopher Sims, based on 6/03 matlab code.  This version 3/27/04.
-### Added dimension labeling, 8/02/04.
+### Added dimension labeling, 8/02/04.  Allow non-square smat, integrate with rfvar3 output, 4.7.10.
   {
     ##-----debug--------
     ##browser()
     ##------------------
+    B <- vout$By
     neq <- dim(B)[1]
     nvar <- dim(B)[2]
     lags <- dim(B)[3]
     dimnB <- dimnames(B)
-    if(dim(smat)[2] != dim(B)[2]) stop("B and smat conflict on # of variables")
-    response <- array(0,dim=c(neq,nvar,nstep+lags-1));
+    if (is.null(smat)) {
+       if (is.null(order) ) order <- 1:neq         
+       smat <- t(pchol(crossprod(vout$u)/dim(vout$u)[1], order[neq:1])) # makes first shock affect all variables
+    }
+    nshock <- dim(smat)[2]
+    if(dim(smat)[1] != dim(B)[1]) stop("B and smat conflict on # of equations")
+    response <- array(0,dim=c(neq,nshock,nstep+lags-1));
     response[ , , lags] <- smat
     response <- aperm(response, c(1,3,2))
     irhs <- 1:(lags*nvar)
     ilhs <- lags * nvar + (1:nvar)
-    response <- matrix(response, ncol=neq)
+    response <- matrix(response, ncol=nshock)
     B <- B[, , seq(from=lags, to=1, by=-1)]  #reverse time index to allow matrix mult instead of loop
     B <- matrix(B,nrow=nvar)
     for (it in 1:(nstep-1)) {
@@ -43,8 +46,8 @@ impulsdtrf <- function(B,smat,nstep)
 ##         for (ilag in 1:min(lags,it-1))
 ##           response[,,it] <- response[,,it]+B[,,ilag] %*% response[,,it-ilag]
 ##       }
-    dim(response) <- c(nvar, nstep + lags - 1, nvar)
-    response <- aperm(response[ , -(1:(lags-1)), ], c(1, 3, 2)) #drop the zero initial conditions; array in usual format
+    dim(response) <- c(nvar, nstep + lags - 1, nshock)
+    response <- aperm(response[ , -(1:(lags-1)), ,drop=FALSE], c(1, 3, 2)) #drop the zero initial conditions; array in usual format
     dimnames(response) <- list(dimnB[[1]], dimnames(smat)[[2]], NULL)
     ## dimnames(response)[2] <- dimnames(smat)[1]
     ## dimnames(response)[1] <- dimnames(B)[2]

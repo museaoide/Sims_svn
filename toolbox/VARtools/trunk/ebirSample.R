87da8e6a1c69fc3fa2c248129f4b599e42deacd3
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2011-04-19 01:54:19 +0000

    continuing repairs to ebirSample, impulsdtrf

--- toolbox/VARtools/trunk/ebirSample.R
+++ toolbox/VARtools/trunk/ebirSample.R
@@ -6,8 +6,8 @@ ebirSample <- function(pdout, horiz, shockf=NULL, ... ) {
   ## We suppose draws are from a reduced form VAR and
   ## shockf is a one-one transformation of smat that
   ## for each i returns a square root of
-  ## crossprod(t(smat[ , , i])), i.e. crossprod(t(shockf(s, ...))) ==
-  ## crossprod(t(s)).  "..." is extra arguments for shockf.
+  ## smat[ , , i]), i.e. crossprod(shockf(s, ...)) ==
+  ## smat.  "..." is extra arguments for shockf.
   ##-------------
   ndraw <- dim(pdout$By)[4]
   if (is.null(shockf)) {
@@ -23,6 +23,6 @@ ebirSample <- function(pdout, horiz, shockf=NULL, ... ) {
   ndraw <- dim(By)[4]
   resp <- array(0, c(nv, nv, horiz, ndraw))
   for (id in 1:ndraw)
-    resp[ , , , id] <-   impulsdtrf(vout=list(By=mxpd$By[, , , id]), smat=mxpd$smat[, , id], nstep=horiz)
+    resp[ , , , id] <-   impulsdtrf(vout=list(By=mxpd$By[, , , id]), smat=t(chol(mxpd$smat[, , id])), nstep=horiz)
   return(resp)
 }

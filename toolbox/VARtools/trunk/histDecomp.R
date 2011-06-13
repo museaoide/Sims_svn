debb13d2242d9abf854574fc73da9cd7e58ea249
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2011-06-13 03:15:13 +0000

    add histdecomp?

---
+++ toolbox/VARtools/trunk/histDecomp.R
@@ -0,0 +1,29 @@
+histDecomp <- function(vout, vts, xdata=NULL, const=TRUE, orthmat=NULL) {
+  if(is.null(orthmat)) orthmat <- chol(cov(vout$u))
+  lags <- dim(vout$By)[3]
+  nv <- dim(vout$By)[1]
+  T <- dim(vts)[1]
+  horiz <- T - lags
+  if (is.null(xdata)) nx <- 0 else nx <- dim(xdata)[2]
+  if (const) nx <- nx + 1
+  ## form deterministic part
+  ydet <- fcast(vts[1:lags, ], vout$By, vout$Bx, xdata, const, horiz)
+  ydecomp <- array(0, c(nv, nv + 1, T))
+  ydecomp[ , nv+1, ] <- t(ydet)
+  orthmati <- solve(orthmat)
+  orthshock <- vout$u[(lags + 1):T, ] %*% orthmati
+  for (iv in 1 : nv) {
+    shocks <- matrix(0, T - lags, nv)
+    shocks[ , iv] <- orthshock[ , iv]
+    shocks <- shocks %*% orthmat
+    ydecomp[ , iv, ] <-  t(fcast(matrix(0, lags, nv), vout$By, vout$Bx, xdata=NULL, const=FALSE, horiz, shocks))
+  }
+  ydecompStacked <- apply(ydecomp, c(1,2), cumsum)
+  ydecompStacked <- aperm(ydecompStacked, c(2, 3, 1))
+  dn2 <- dimnames(vout$By)[[1]]
+  if (const) dn2 <- c(dn2, "const")
+  dimnames(ydecomp) <- list(dimnames(vout$By)[[1]], dn2, NULL)
+  dimnames(ydecompStacked) <- dimnames(ydecomp)
+  return(list(ydec=ydecomp, ydecStack=ydecompStacked))
+}
+  

32b5a85a45de196affec7da23e596f95baeb1a47
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2013-02-14 02:09:14 +0000

    typo fix in MHchoice.R, Mchoice.R

--- toolbox/MCMC/MHchoice.R
+++ toolbox/MCMC/MHchoice.R
@@ -1,4 +1,4 @@
 MHchoice <- function(lhprop, lhtarget, theta0, theta1) {
-  rho <- lhtarget(theta1) * lhprop(theta0)/(lhtarget(theta0) * lhtarget(theta1))
+  rho <- lhtarget(theta1) * lhprop(theta0)/(lhtarget(theta0) * lhprop(theta1))
   if( runuf(1) > rho ) return (theta1) else return(theta0)
 }

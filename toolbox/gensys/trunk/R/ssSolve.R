434a0240983b4d67c47bc4c36d7951bb53108e43
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2013-02-20 19:23:14 +0000

    add a comment to ssSolve

--- toolbox/gensys/trunk/R/ssSolve.R
+++ toolbox/gensys/trunk/R/ssSolve.R
@@ -4,6 +4,7 @@ ssSolve <- function(ex, x0, param, crit=1e-7, itmax=20, verbose=TRUE, alpha=1e-3
   shockval <- rep(0, length(shocknames))
   names(shockval) <- shocknames
   fss <- derivVec(ex2, attr(ex2, "vlist"), c(param, shockval))
+  ## fss returns the value of the ex2 system expressions, and also their derivatives.  csolve() knows how to use such a function.
   csout <- csolve(fss, x0, gradfun=fss, crit=crit, itmax=itmax, verbose=verbose, alpha=alpha, delta=delta, long=long)
   xss <- structure(as.vector(csout$x), names=dimnames(csout$x)[[1]], param=param)
   return(list(xss=xss, csout=csout, fss=fss))

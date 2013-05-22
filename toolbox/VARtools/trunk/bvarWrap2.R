734b5dd436d3cec8dd8533b93649965a8f0df4b2
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2013-05-22 01:36:52 +0000

    Add bvarwrap2.R.  move vdecomp.R to trunk.

---
+++ toolbox/VARtools/trunk/bvarWrap2.R
@@ -0,0 +1,40 @@
+bvarWrap2 <- function(x, prior) {
+    ## For returning detailed results, after convergence, use second return
+    ## below and comment out the simple return(lh).
+    Tsigbrk <- invTime(c(1979.75, 1983.0, 2008.0, 2010.0),  slimdata4)
+    Lags <- 6
+    nv <- 6
+    enddata <- 2007.75
+    T <- dim(window(slimdata4, end=enddata))[1]
+    Tsigbrk <- c(0, Tsigbrk)
+    Tsigbrk <- c(Tsigbrk[Tsigbrk < T], T)
+    nsig <- length(Tsigbrk) - 1
+    A <- matrix(0, nv, nv)
+    dgx <- seq(1, nv^2, by=nv+1)
+    A[dgx] <-  1
+    A[-dgx] <- x[1:(nv^2 - nv)]
+    lmd <- matrix(0, nv, nsig)
+    lmd[ , ] <- x[nv^2 - nv + 1:(nv*nsig)]
+    sigfac <- array(0, c(nv, nv, T))
+    for (isig in 1:nsig) {
+        sigval <- chol(crossprod(t(A %*% diag(exp(lmd[ , isig])))))
+        sigfac[ , , (Tsigbrk[isig]+1):Tsigbrk[isig+1]] <- sigval
+    }
+    vout <- rfvarKF(ydata = window(slimdata4, end=enddata), lags = Lags, sigfac = sigfac, prior = prior)
+    lh=-sum(vout$lh)
+    attr(lh,"prior") <- prior
+    attr(lh,"sigfac") <- sigfac[ , , Tsigbrk[-1]]
+    attr(lh, "T") <- T
+    attr(lh, "data") <- "slimdata4"
+    ## prior on lambda's, to stay away from zeros.
+    lmscale <- .002
+    nlmd <- length(c(lmd))
+    lplmd <- nlmd * (log(2) -  2 * log(lmscale)) + 3 * sum(lmd) - 3 * sum(log(1 + lmscale^(-2) * exp(2 * lmd)))
+    ## penalize highly unstable roots
+    ev <- eigen(sysmat(vout$By))$values
+    ev <- sum(abs(ev[abs(ev) > 1] - 1))^2*1e3 #(so one root of 1.03 penalized by .9 (weak)
+    lh <- lh+ev - lplmd
+    attr(lh, "penalty") <- ev
+    ##return(lh)
+    return(list(lh=lh, vout=vout, A=A, lmd=exp(lmd))) # uncomment for analyzing output 
+}

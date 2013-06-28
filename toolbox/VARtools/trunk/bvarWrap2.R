c33b7e2f102e6c660456fe02d57cfbc37703e25e
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2013-06-28 19:30:31 +0000

    add rfvarKFx, new bvarWrap2 (w. block kf)

--- toolbox/VARtools/trunk/bvarWrap2.R
+++ toolbox/VARtools/trunk/bvarWrap2.R
@@ -1,12 +1,13 @@
-bvarWrap2 <- function(x, prior) {
+bvarWrap2 <- function(x, prior, verbose=FALSE) {
     ## For returning detailed results, after convergence, use second return
     ## below and comment out the simple return(lh).
     Tsigbrk <- invTime(c(1979.75, 1983.0, 2008.0, 2010.0),  slimdata4)
+    ## here, Tsigbrk is when new sig starts; below we shift it back to be last obs with old sig.
     Lags <- 6
     nv <- 6
     enddata <- 2007.75
     T <- dim(window(slimdata4, end=enddata))[1]
-    Tsigbrk <- c(0, Tsigbrk)
+    Tsigbrk <- c(0, Tsigbrk - 1)
     Tsigbrk <- c(Tsigbrk[Tsigbrk < T], T)
     nsig <- length(Tsigbrk) - 1
     A <- matrix(0, nv, nv)
@@ -15,15 +16,13 @@ bvarWrap2 <- function(x, prior) {
     A[-dgx] <- x[1:(nv^2 - nv)]
     lmd <- matrix(0, nv, nsig)
     lmd[ , ] <- x[nv^2 - nv + 1:(nv*nsig)]
-    sigfac <- array(0, c(nv, nv, T))
-    for (isig in 1:nsig) {
-        sigval <- chol(crossprod(t(A %*% diag(exp(lmd[ , isig])))))
-        sigfac[ , , (Tsigbrk[isig]+1):Tsigbrk[isig+1]] <- sigval
-    }
-    vout <- rfvarKF(ydata = window(slimdata4, end=enddata), lags = Lags, sigfac = sigfac, prior = prior)
+    sigfac <- array(0, c(nv, nv, nsig))
+    for (isig in 1:nsig) 
+        sigfac[ , , isig] <- exp(-.5 * lmd[ , isig]) * t(A)
+    vout <- rfvarKFx(ydata = window(slimdata4, end=enddata), lags = Lags, sigfac = sigfac, Tsigbrk=Tsigbrk, prior = prior)
     lh=-sum(vout$lh)
     attr(lh,"prior") <- prior
-    attr(lh,"sigfac") <- sigfac[ , , Tsigbrk[-1]]
+    attr(lh,"sigfac") <- sigfac
     attr(lh, "T") <- T
     attr(lh, "data") <- "slimdata4"
     ## prior on lambda's, to stay away from zeros.
@@ -33,8 +32,10 @@ bvarWrap2 <- function(x, prior) {
     ## penalize highly unstable roots
     ev <- eigen(sysmat(vout$By))$values
     ev <- sum(abs(ev[abs(ev) > 1] - 1))^2*1e3 #(so one root of 1.03 penalized by .9 (weak)
-    lh <- lh+ev - lplmd
+    lh <- lh + ev - lplmd
     attr(lh, "penalty") <- ev
-    return(lh)
-    ##return(list(lh=lh, vout=vout, A=A, lmd=exp(lmd))) # uncomment for analyzing output 
+    if(verbose)
+        return(list(lh=lh, vout=vout, A=A, lmd=exp(lmd))) # uncomment for analyzing output
+    else
+        return(lh)
 }

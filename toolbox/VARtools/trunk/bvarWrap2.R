8d1aab0d11027e350273fb7d7f0d6fffb854c7d9
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2013-07-26 18:40:19 +0000

    bvarWrap2 with non-x KF

--- toolbox/VARtools/trunk/bvarWrap2.R
+++ toolbox/VARtools/trunk/bvarWrap2.R
@@ -1,46 +1,64 @@
 bvarWrap2 <- function(x, verbose=FALSE) {
     ## For returning detailed results, set verbose=TRUE
-    ## Tsigbrk <- invTime(c(1979.75, 1983.0, 2008.0, 2010.0),  slimdata4)
-    Tsigbrk <- invTime(c(1979.75, 1983.0, 2008.0),  slimdata4)
+    dataseries <- slimdata12
+    ## Tsigbrk <- invTime(c(1979.75, 1983.0, 2008.0, 2010.0),  dataseries)
+    Tsigbrk <- invTime(c(1979+2/3, 1983.0, 2008.0),  dataseries)
     ## here, Tsigbrk is when new sig starts; below we shift it back to be last obs with old sig.
     Lags <- 6
     nv <- 6
     ## enddata <- 2007.75
-    enddata <- end(slimdata4)
-    T <- dim(window(slimdata4, end=enddata))[1]
+    enddata <- end(dataseries)
+    T <- dim(window(dataseries, end=enddata))[1]
     Tsigbrk <- c(0, Tsigbrk - 1)
     Tsigbrk <- c(Tsigbrk[Tsigbrk < T], T)
     nsig <- length(Tsigbrk) - 1
     A <- matrix(0, nv, nv)
     dgx <- seq(1, nv^2, by=nv+1)
+    a123x <- nv * 2 + 1:2               #identifying constraint: A[ , 3] is M policy
     A[dgx] <-  1
-    A[-dgx] <- x[1:(nv^2 - nv)]
+    A[a123x] <- 0                       #a123x added after 13.7.23a
+    A[-c(dgx, a123x)] <- x[1:(nv^2 - nv - 2)]   
     lmd <- matrix(0, nv, nsig)
-    lmd[ , ] <- x[nv^2 - nv + 1:(nv*nsig)]
+    ## lmd[ , ] <- x[nv^2 - nv + 1:(nv*nsig)]
+    lmd[ , ] <- x[nv^2 - nv - 2 + 1:(nv*nsig)] #shorter param vector with a123x constraint
     sigfac <- array(0, c(nv, nv, nsig))
     for (isig in 1:nsig) 
         sigfac[ , , isig] <- exp(-.5 * lmd[ , isig]) * t(A)
     sig0 <- crossprod(sigfac[ , , 1])
-    vnames <- dimnames(slimdata4)[[2]]
+    vnames <- dimnames(dataseries)[[2]]
     dimnames(sig0) <- list(vnames, vnames)
     ## --------- set up prior parameters ---------------
     mnprior <- list(tight=5, decay=.5)
     ## vprior <- list(sig=sqrt(diag(sig0)), w=0)
-    vprior <- list(sig=c(rep(.01,5),.1), w=0)
-    names(vprior$sig) <- dimnames(slimdata4)[[2]]
+    vprior <- list(sig=rep(.01,nv), w=0)
+    names(vprior$sig) <- dimnames(dataseries)[[2]]
+    ## ----------------prior on A
+    asig <- .25
+    asd <- outer(vprior$sig, 1/vprior$sig)
+    allh <- -.5 * sum((A / asd)[-c(dgx, a123x)]^2)/asig^2 - sum(log(asd[-c(dgx, a123x)])) -
+        .5 * (nv^2 - length(c(dgx, a123x))) * (log(2 * pi) + 2 * log(asig))
+    ##--------------------------
     urprior <- list(lambda=5, mu=1)
-    ybar <- apply(slimdata4[1:6, ], 2, mean, na.rm = TRUE)
+    ybar <- apply(dataseries[1:6, ], 2, mean, na.rm = TRUE)
     sigfix <- diag(vprior$sig^2)
-    ## nstat <- rep(TRUE, 6)
-    nstat <- c(rep(TRUE, 4), FALSE, FALSE)
+    nstat <- rep(TRUE, 6)
+    ## nstat <- c(rep(TRUE, 4), FALSE, FALSE)
     dimnames(sigfix) <- list(vnames,vnames)
     prior <- varpriorN(nv, nx = 1, lags = Lags, mnprior = mnprior, vprior = vprior, urprior = urprior, ybar = ybar, sigfix=sigfix, nstat=nstat)
-    vout <- rfvarKFx(ydata = window(slimdata4, end=enddata), lags = Lags, sigfac = sigfac, Tsigbrk=Tsigbrk, prior = prior)
+    ## ------------- rfvarKFx does KF in time blocks.  Faster if blocks are not too big
+    ##                relative to # of params.  But O(T^3), while rfvarKF is O(T)
+    ## vout <- rfvarKFx(ydata = window(dataseries, end=enddata), lags = Lags,
+    ##                 sigfac = sigfac, Tsigbrk=Tsigbrk, prior = prior)
+    ## -----------------
+    sigfacx <- array(0, c(nv, nv, T))
+    for (isig in 1:nsig) sigfacx[ , , (Tsigbrk[isig] + 1):Tsigbrk[isig+1]] <- sigfac[ , , isig]
+    vout <- rfvarKF(ydata = window(dataseries, end=enddata), lags = Lags,
+                    sigfac = sigfacx, prior = prior)
     lh <- -sum(vout$lh)                 #Note sign flip, for minimization
     attr(lh,"prior") <- list(mnprior=mnprior, vprior=vprior, urprior=urprior, ybar=ybar, sigfix=sigfix, nstat=nstat)
     attr(lh,"sigfac") <- sigfac
     attr(lh, "T") <- T
-    attr(lh, "data") <- "slimdata4"
+    attr(lh, "data") <- "dataseries"
     ## prior on lambda's, to stay away from zeros.
     ## lmscale <- .002
     ## nlmd <- length(c(lmd))
@@ -54,7 +72,7 @@ bvarWrap2 <- function(x, verbose=FALSE) {
     ##-----------------------------
     ev <- 0
     ##-------------------------------------------------------------------------
-    lh <- lh + ev - lplmd                     # correct marginal posterior pdf | lmd, A
+    lh <- lh + ev - lplmd - allh                    # correct marginal posterior pdf | lmd, A
     ##-------------------------------------------------------------------------
     ## dsig <- diag(chol(vout$Vb, pivot=TRUE))
     ## if (any(dsig <= 0)) {
@@ -67,7 +85,7 @@ bvarWrap2 <- function(x, verbose=FALSE) {
     attr(lh, "penalty") <- ev
     if(verbose) {
         ## form in-sample residuals---------
-        u <- fcastMany(slimdata4, vout$By, vout$Bx, horiz=1:8)
+        u <- fcastMany(dataseries, vout$By, vout$Bx, horiz=1:8)
         u1std <- matrix(0, dim(u$u)[1]+Lags, dim(u$u)[3])
         for (isig in 1:nsig) {
             u1std[(Tsigbrk[isig] + 1):Tsigbrk[isig + 1], ] <-
@@ -75,7 +93,7 @@ bvarWrap2 <- function(x, verbose=FALSE) {
                     diag(1/sqrt(diag(crossprod(sigfac[ , , isig]))))
             u1std <- ts(u1std, start=tsp(u$u)[1], freq=tsp(u$u)[3])
         }
-        return(list(lh=lh, vout=vout, A=A, lmd=exp(-.5*lmd), llmd = lmd, f=u$fc, u=u$u, u1std=u1std)) #llmd included because exp(-.5*lmd) might lose precision.
+        return(list(lh=lh, vout=vout, A=A, lmd=exp(-.5*lmd), llmd = lmd, f=u$fc, u=u$u, u1std=u1std, asig=asig)) #llmd included because exp(-.5*lmd) might lose precision.
     } else {
         return(lh)
     }

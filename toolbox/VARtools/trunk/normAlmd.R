bf5245cf379ecaa0e5aecb722c7ad6275727ab00
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2014-07-12 16:55:15 +0000

    std update from home

--- toolbox/VARtools/trunk/normAlmd.R
+++ toolbox/VARtools/trunk/normAlmd.R
@@ -1,6 +1,12 @@
 normAlmd <- function(Aml, lmdml, A, lmd) {
+    if(is.null(dim(lmd))) lmd <- matrix(lmd, length(lmd), 1)
+    if(is.null(dim(lmdml))) lmdml <- matrix(lmdml, length(lmdml), 1)   
     nsig <- dim(lmd)[2]
     nv <- dim(lmd)[1]
+    ## normalize diagonal of A, just in case
+    sf <- diag(A)
+    A <- (1/sf) * A
+    lmd <- lmd - 2 * c(log(abs(sf)))        #vector of log sf's gets reused, col by col
     Alml <- array(0, c(nv, nv, nsig))
     Al <- Alml
     for (il in 1:nsig) {
@@ -9,32 +15,36 @@ normAlmd <- function(Aml, lmdml, A, lmd) {
     }
     Alml <- matrix(Alml, nv)
     Al <- matrix(Al, nv)
-    xp <- Al %*% t(Alml)
+    xp <- abs(Al %*% t(Alml))
+    ## xp <- abs(cor(t(Al), t(Alml)))
     ## Algorithm tries reordering up to nv times to find an invariant ordering,
     ## then gives up and returns nv'th reordering and noloop=FALSE
     ordrng <- 1:nv
     crit <- vector("numeric", nv)
-    Alold <- Al
     noloop <- 0
     for (ntrial in 1:nv) {
-        ## Make any switch with 1 that increases trace, then any with 2, etc.
+        thisOrdrng <- 1:nv
+        ## Make any switch with 1 that increases trace(xp), then any with 2, etc.
         for (iv in 1:nv) {
             for (iv2 in iv:nv) {
                 crit[iv2] <- xp[iv2,iv] - xp[iv,iv] + xp[iv,iv2] - xp[iv2,iv2]
             }
             idtr <- which.max(crit[iv:nv])
-            ordrng[iv:nv] <- ordrng[iv:nv][c(idtr , (1:(nv-iv+1))[-idtr])]
-            Al <- Al[ordrng, ]
-            xp <- xp[ordrng, ]
+            newiv <- thisOrdrng[iv:nv][idtr]
+            thisOrdrng[iv:nv][idtr] <- thisOrdrng[iv]
+            thisOrdrng[iv] <- newiv
+            Al <- Al[thisOrdrng, ]
+            xp <- xp[thisOrdrng, ]
         }
-        if (isTRUE(all.equal(Al, Alold))) {
+        ordrng <- ordrng[thisOrdrng]
+        if (all(thisOrdrng == 1:nv)) {
             noloop <- ntrial
             break
-            Alold <- Al
         }
     }
-    sf <- diag(Al)
-    Al <- (1/sf) * Al
-    lmd <- sf * lmd
-    return(list(Anormed=Al , lmdnormed=lmd, ordrng=ordrng, noloop=noloop))
+    A <- A[ordrng, ]
+    sf <- diag(A)
+    A <- (1/sf) * A
+    lmd <- lmd[ordrng, ] - 2 *c(log(abs(sf)))
+    return(list(Anormed=A , lmdnormed=lmd, ordrng=ordrng, noloop=noloop))
 }
--- toolbox/VARtools/trunk/plotir.R
+++ toolbox/VARtools/trunk/plotir.R
@@ -17,15 +17,15 @@ plotir <- function(resp, var, shock, type=c("multiple","single"),main="Impulse r
     pr <- resp[var,shock, , drop=FALSE]
     ybound <- matrix(0,2,length(var))
     for (iv in 1:length(var)) ybound[ , iv] <- range(pr[iv, , ])
-    if(vfirst) pr <- aperm(pr, c(2,1,3))
+    if(!vfirst) pr <- aperm(pr, c(2,1,3))
     nr <- dim(pr)[1]
     nc <- dim(pr)[2]
     np <- dim(pr)[3]
     layout(matrix(1:(nr * nc), nr, nc))
     for (ic in 1:nc) {
-        if (nr > 1) {
-            for (ir in 1:(nr-1))
-                plot(0:(np-1), pr[ir, ic, ], type="l", ylim=ybound[ , if(vfirst) ir else ic], frame=TRUE)    
-        }
+    ##    if (nr > 1) {
+            for (ir in 1:nr)
+                plot(0:(np-1), pr[ir, ic, ],bty="l", type="l", ylim=ybound[ , if(vfirst) ir else ic], frame=TRUE, ylab=if(ic==1) dimnames(pr)[[1]][ir] else "", xlab="", main=if(ir ==1) dimnames(pr)[[2]][ic] else "")    
+    ##    }
     }
 }

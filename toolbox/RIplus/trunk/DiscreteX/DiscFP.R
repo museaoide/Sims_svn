e8a62cd9b47f3857ee5fd56aab73587d210eddd3
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2015-03-07 15:59:10 +0000

    generalizing DiscFP to mvariate, general U

--- toolbox/RIplus/trunk/DiscreteX/DiscFP.R
+++ toolbox/RIplus/trunk/DiscreteX/DiscFP.R
@@ -1,4 +1,5 @@
-DiscFP <- function(param, gy, y, U, alph, nit=10, crit=1e-7, ...) {
+DiscFP <- function(param, gy, y, U, xfcn, alph, nit=10, crit=1e-7, ...) {
+  ##   ## xfcn:   A function of ygivenx (cond'l prob matrix), y and U returning optimal x
   screp <- crit + 10
   itct = 0
   while (screp > crit && itct < nit) {
@@ -10,7 +11,8 @@ DiscFP <- function(param, gy, y, U, alph, nit=10, crit=1e-7, ...) {
     pold <- param[2:nx]
     pold <- c(pold, 1 - sum(pold))
     pnew <- Dout$pnew
-    xnew <- Dout$ygivenx %*% y #-------xnew solves E[Dxu %*% y = 0] This is for UmvTrack
+    ## xnew <- Dout$ygivenx %*% y #-------xnew solves E[Dxu %*% y = 0] This is for UmvTrack
+    xnew <- xfcn(Dout$ygivenx, y, U)
     if ( length(pnew) != length(pold)) print(itct)
     screp <- sum(abs(c(pnew - pold, xnew - param[-(1:nx)])))
     param <- c(nx, pnew[-nx], xnew)

dff9edae18137590a93a1a8efffae13fc5841d1c
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2015-03-21 01:15:18 +0000

    pxFromCsout and DiscZ (csolve arg) added

---
+++ toolbox/RIplus/trunk/DiscreteX/DiscZ.R
@@ -0,0 +1,46 @@
+DiscZ <- function(x, nx, gy, y, U, xfcn, alph, ...) {
+    ##
+    ## xfcn:   A function of ygive nx (cond'l prob matrix), y and U returning optimal x
+    ## This only works for CapM, because need to check legality of x and p
+    ## parameters for that model.
+    ## must be able to evaluate with x a matrix, with each col an arg
+    require(abind)
+    if (is.null(dim(x)) ) {
+        nargcol <- 1
+        x <- matrix(x, ncol=1)
+    } else {
+        nargcol <- dim(x)[2]
+    }
+    pold <- matrix(x[1:(nx-1), ], ncol=nargcol)
+    pold <- rbind(pold, 1 - apply(pold, 2, sum))
+    ny <- dim(y)[2]
+    xold <- array(x[-(1:(nx-1)), ], c(nx, ny - 1, nargcol ))
+    sxold <-  1 - apply(xold, c(1,3), sum)
+    sxold <- array(sxold, c(nx , 1, nargcol))
+    xold <- abind(xold, sxold, along=2) 
+    pnew <- matrix(0, nx, nargcol)
+    xnew <- array(0, c(nx, ny, nargcol))
+    screp <- matrix(1e20, dim(x)[1], nargcol)
+    ## DiscPObjXmv needs pold truncated, but not xold
+    for (icol in 1:nargcol) {
+        if (!any(pold[ , icol] < 0)) {
+            xc <- c(nx, pold[1:(nx - 1), icol], xold[ , , icol])
+            Dout <- DiscPObjXmv(xc, gy, y, U=U, alph=alph,...)    
+            pnew[ , icol] <- Dout$pnew
+            ## xnew <- Dout$ygivenx %*% y #-------xnew solves E[Dxu %*% y = 0] This is for UmvTrack
+            xnew[ , , icol] <- xfcn(Dout$ygivenx, y)
+            screp[ , icol] <- c((pnew[-nx , icol] - pold[-nx , icol]), (xnew - xold)[ , 1:(ny-1), icol])
+            ##otherwise leave the 1e20 values in place in that column
+        }
+    }
+    ## x <- c(nx, pnew[-nx], xnew)
+    ## if (itct %% 100 == 0) {
+    ##   print(itct)
+    ##   print(screp)
+    ##   print(Dout$obj)
+    ##   print(Dout$pnew)
+    ##   print(xnew)
+    ## }
+    return(screp)
+}
+
---
+++ toolbox/RIplus/trunk/DiscreteX/pxFromCsout.R
@@ -0,0 +1,8 @@
+pxFromCsout <- function(cx, nx) {
+    p <- cx[1:(nx-1)]
+    p <- c(p, 1 - sum(p))
+    x <-matrix(cx[-(1:(nx-1))], nrow=nx)
+    sx <- apply(x, 1, sum)
+    x <- cbind(x, sx)
+    return(list(p=p, x=x))
+}

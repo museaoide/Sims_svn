edb9ab25ea7851d824473f08bcb7accde97ce091
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2015-04-04 01:05:03 +0000

    programming for 2p modela

--- toolbox/RIplus/trunk/DiscreteX/DiscZ.R
+++ toolbox/RIplus/trunk/DiscreteX/DiscZ.R
@@ -1,9 +1,8 @@
 DiscZ <- function(x, nx, gy, y, U, xfcn, alph, ...) {
     ##
     ## xfcn:   A function of ygive nx (cond'l prob matrix), y and U returning optimal x
-    ## This only works for CapM, because need to check legality of x and p
-    ## parameters for that model.
-    ## must be able to evaluate with x a matrix, with each col an arg
+    ## There is a special version for CapM, because need to impose that rows of x sum to one in that model
+    ## Must be able to evaluate with x a matrix, with each col an arg
     require(abind)
     if (is.null(dim(x)) ) {
         nargcol <- 1
@@ -14,10 +13,13 @@ DiscZ <- function(x, nx, gy, y, U, xfcn, alph, ...) {
     pold <- matrix(x[1:(nx-1), ], ncol=nargcol)
     pold <- rbind(pold, 1 - apply(pold, 2, sum))
     ny <- dim(y)[2]
-    xold <- array(x[-(1:(nx-1)), ], c(nx, ny - 1, nargcol ))
-    sxold <-  1 - apply(xold, c(1,3), sum)
-    sxold <- array(sxold, c(nx , 1, nargcol))
-    xold <- abind(xold, sxold, along=2) 
+    ## if capm
+    ## xold <- array(x[-(1:(nx-1)), ], c(nx, ny - 1, nargcol ))
+    ## sxold <-  1 - apply(xold, c(1,3), sum)
+    ## sxold <- array(sxold, c(nx , 1, nargcol))
+    ## xold <- abind(xold, sxold, along=2)
+    ## else
+    xold <- array(x, c(nx, ny, nargcol))
     pnew <- matrix(0, nx, nargcol)
     xnew <- array(0, c(nx, ny, nargcol))
     screp <- matrix(1e20, dim(x)[1], nargcol)
@@ -29,7 +31,11 @@ DiscZ <- function(x, nx, gy, y, U, xfcn, alph, ...) {
             pnew[ , icol] <- Dout$pnew
             ## xnew <- Dout$ygivenx %*% y #-------xnew solves E[Dxu %*% y = 0] This is for UmvTrack
             xnew[ , , icol] <- xfcn(Dout$ygivenx, y)
-            screp[ , icol] <- c((pnew[-nx , icol] - pold[-nx , icol]), (xnew - xold)[ , 1:(ny-1), icol])
+            ## if !capm
+            screp[ , icol] <- c((pnew[-nx , icol] - pold[-nx , icol]), xnew - xold)
+            ## else
+            ## screp[ , icol] <- c((pnew[-nx , icol] - pold[-nx , icol]), (xnew - xold)[ ,1:(ny-1),] )
+            ## fi
             ##otherwise leave the 1e20 values in place in that column
         }
     }

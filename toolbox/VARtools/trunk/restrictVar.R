aac0cf0ee2a6d2be41d6f13c1195e8a82455441c
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2015-05-15 12:41:50 +0000

    fixing restrictVAR

--- toolbox/VARtools/trunk/restrictVar.R
+++ toolbox/VARtools/trunk/restrictVar.R
@@ -1,29 +1,64 @@
-restrictVAR <- function(vout, type=c("3", "KF","SVhtskd"), rmat=NULL, yzrone=NULL, xzrone=NULL,
-                        const=NULL, cyzr=NULL, cxzr=NULL) {
-    ## restrictions can be specified as rows of rmat, with coefficients applied to elements of By and Bx
-    ## stacked as they are in xxi (and then repeated across the equation index), or they can be specified
-    ## in yzrone, xzrone.  Each zero element of yzrone or xzrone generates a restriction that sets the corresponding
-    ## coefficient in By or Bx to zero (or to a constant, if !is.null(const)).  Both kinds of restrictions
-    ## can be non-trivial in the same call.
-    ##-------------------------------------------
-    ## type:     vout as from rfvar3 ("3") or as from rfvarKF ("KF")
-    ## const:    the right hand side of rmat %*% coeff = const, not the constant in the var.
-    ## cyzr, cxzr:  If using yzrone, xzrone with non-trivial constants, leave const=NULL and specify
-    ##           constants with cyzr and cxzr
-    ##------------------------------------------
-    ## sc:       The Schwarz criterion rejects the restriction if the chisq value plus the sc value
-    ##           is positive.  This version of the sc is scale-sensitive.  Variables with higher
-    ##           variance are penalized more strongly, as with a prior that expects higher-variance
-    ##           variables to explain more variance.
-    ##
-    ##------------------------------------------------
-    ## Note 2013-3-4:  Try eliminating scale effects by converting X'X to correlation matrix
+#' restrict VAR
+#'
+#' Posterior odds on linear restrictions on a VAR.
+#'
+#' The calculations are done as though the prior on the restricted space is
+#' proporitonal to the prior density on the unrestricted space, confined to
+#' the restricted space.  This makes sense if the restriction is approximating
+#' a restriction to a small neighborhood of the restricted space.
+#' For the case of \code{SVARhtskdmdd} output as input to this function, the
+#' results are conditional on the modal \code{A0} and \code{lmd}. For the
+#' case of \code{mgnldensity()} output, the results condition on the modal
+#' residual covariance matrix, as if it were known.  
+#' Restrictions can be specified as rows of rmat, with coefficients applied to
+#' elements of \code{By} and \code{Bx} stacked as they are in \code{xxi} (and
+#' then repeated across the equation index), or
+#' they can be specified in \code{yzrone}, \code{xzrone}.  Each zero element
+#' of \code{yzrone} or \code{xzrone} generates a restriction that sets the
+#' corresponding coefficient in \code{By} or \code{Bx} to zero (or to a
+#' constant, if \code{!is.null(const)}).  Both kinds of restrictions can be
+#' non-trivial in the same call.
+#'
+#' @param fitdata Output from a var estimate with a proper prior from
+#'                \code{mgnldnsty()} or \code{SVARhtskmdd()})
+#' @param type Either \code{mdd} or \code{SVmdd}, depending on which
+#'             program produced \code{fitdata}.
+#'
+#' @param const    The right hand side of \code{rmat %*% coeff = const}, not
+#'                 the constant in the var.
+#' 
+#' @param cyzr,cxzr  If using \code{yzrone}, \code{xzrone} with non-trivial
+#'                   constants, leave \code{const=NULL} and specify constants
+#'                   with \code{cyzr} and \code{cxzr}
+#'
+#' @param sc The posterior odds ratio.  It will be similar, at least in sign,
+#'           to the Schwarz criterion on small models, but if large numbers
+#'           of coefficients are being tested at once, the prior, which the
+#'           Schwarz criterion ignores, may be very important.
+#'------------------------------------------------
+#' @return  A list with elements
+#'   \describe{
+#'    \item{chiSquared}{The usual chi-squared statistic}
+#'    \item{df}{Degrees of freedom of the chi-squared statistic (number of
+#'             restrictions)}
+#'    \item{sc}{Posterior odds ratio against the restriction.)
+#'    \item{pval}{p-value of the chi-squared}
+#'    \item{ singxxi,singsig}{If either of these are TRUE, there might be
+#'     numerical error}
+#'   }
+#' @import tensor
+#' @export
+#' 
+restrictVAR <- function(fitdata, type=c("mdd", "SVmdd"), rmat=NULL,
+                        yzrone=NULL, xzrone=NULL,const=NULL, cyzr=NULL,
+                        cxzr=NULL) {
     if (length(type) > 1) type <- type[1]
-    if (type == "SVhtskd") {
-        require("tensor")
-        bvw <- vout
-        vout <- bvw$vout$var
-    }
+##     if (type == "SVmdd") {
+## ##      require("tensor")  #not needed if has been packaged, because of @import
+##         bvw <- vout
+##         vout <- bvw$vout$var
+##     }
+    vout <- fitdata$var
     neq <- dim(vout$By)[1]
     ny <- dim(vout$By)[2]
     lags <- dim(vout$By)[3]
@@ -58,17 +93,15 @@ restrictVAR <- function(vout, type=c("3", "KF","SVhtskd"), rmat=NULL, yzrone=NUL
     if (max(abs(svdr$d)) > 1e10 * min(abs(svdr$d))){
         error("restrictions not full rank")
     }
-    ## Note that t(rv) spans the same space as rmat, so the restrictiosn are crossprod(v,coeffs)=gamma
-    ## rv <- svdr$v    #2013.5.9
-    T <- if (type == "3" || type == "SVhtskd") dim(vout$u)[1] else dim(vout$fcsterr)[1]
-    if (type == "3") {
+    T <- dim(vout$u)[1]
+    if (type == "mdd")
         sig <- cov(vout$u)
         svdsig <- svd(sig)
         singsig <- (max(svdsig$d) > 1e10 * min(svdsig$d))
         if(singsig) warning("Near singular sig matrix in restrictVAR")
         svdxxi <- svd(vout$xxi)
         singxxi <- (max(svdxxi$d) > 1e10 * min(svdxxi$d))
-        ## if(singxxi) warning("Near singular xxi matrix in restrictVAR")
+        if(singxxi) warning("Near singular xxi matrix in restrictVAR")
         ## schwarz <- rmat %*% kronecker(svdsig$u %*% diag(1/sqrt(svdsig$d)), svdxxi$u %*% diag(1/sqrt(svdxxi$d)))
         ##schwarz <- kronecker((1/sqrt(svdsig$d)) * t(svdsig$u), (1/sqrt(svdxxi$d)) * t(svdxxi$u)) %*% rv  #2013.5.9
         ## sqrtVb <- kronecker(sqrt(svdsig$d) * t(svdsig$u), 1/sqrt(svdxxi$d)) * t(svdxxi$u)
@@ -79,21 +112,21 @@ restrictVAR <- function(vout, type=c("3", "KF","SVhtskd"), rmat=NULL, yzrone=NUL
         sqrtVbC <- sqrtVb %*% diag(1/sqrt(T * dgVb))
         lndetVb <- sum(log(svdsig$d)) * dim(vout$xxi)[1] + sum(log(svdxxi$d)) * dim(sig)[1]
         lndetVbC <- lndetVb - sum(log(dgVb * T))
-    } else if (type == "KF") {          #type=="KF"
-        svdVb <- svd(vout$Vb)
-        sqrtVb <- sqrt(diag(svdVb$d)) %*% t(svdVb$u)
-        dgVb <- diag(vout$Vb)
-        rmatC <- rmat %*% diag(sqrt(T * dgVb))
-        sqrtVbC <- sqrtVb %*% diag(1/sqrt(T * dgVb))
-        lndetVb <- sum(log(svdVb$d))
-        lndetVbC <- lndetVb - sum(log(dgVb * T))
-        ## schwarz <- rmat %*% svdVb$u %*% diag(1/sqrt(svdVb$d)) #below is more efficient version for large Vb
-        ## schwarz <- (1/sqrt(svdVb$d)) * (t(svdVb$u) %*% rv)
-    } else {                            #type="SVhtskd"
-        nv <- dim(vout$By)[1]
+    ## } else if (type == "KF") {          #type=="KF"
+    ## svdVb <- svd(vout$Vb)
+    ## sqrtVb <- sqrt(diag(svdVb$d)) %*% t(svdVb$u)
+    ## dgVb <- diag(vout$Vb)
+    ## rmatC <- rmat %*% diag(sqrt(T * dgVb))
+    ## sqrtVbC <- sqrtVb %*% diag(1/sqrt(T * dgVb))
+    ## lndetVb <- sum(log(svdVb$d))
+    ## lndetVbC <- lndetVb - sum(log(dgVb * T))
+    ## ## schwarz <- rmat %*% svdVb$u %*% diag(1/sqrt(svdVb$d)) #below is more efficient version for large Vb
+    ## ## schwarz <- (1/sqrt(svdVb$d)) * (t(svdVb$u) %*% rv)
+    } else {            # (type == "SVmdd")
+        neq <- dim(vout$By)[1]
         nX <- dim(vout$xxi)[1]
-        Vb <- matrix(0, nX * nv, nX * nv)
-        for (iq in 1:nv) {
+        Vb <- matrix(0, nX * neq, nX * neq)
+        for (iq in 1:neq) {
             Vb[nX * (iq-1) + 1:nX, nX * (iq-1) + 1:nX] <- vout$xxi[ , , iq]
         }
         A0i <- solve(bvw$A)

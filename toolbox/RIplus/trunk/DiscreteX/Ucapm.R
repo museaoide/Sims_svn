82499104fa790490ea781a641eb9887c96ad2820
chris <chris@38395b84-9d17-0410-abe6-9659aa5f9712>
2015-07-01 12:49:53 +0000

    RIplus new capm

--- toolbox/RIplus/trunk/DiscreteX/Ucapm.R
+++ toolbox/RIplus/trunk/DiscreteX/Ucapm.R
@@ -1,7 +1,11 @@
-Ucapm <- function(a,b) {
-  gamma <- .001
-  xy <- crossprod(a,b)
-  u <- xy - .5 * xy^2 - .5 * gamma * crossprod(a[-1])
+Ucapm <- function(theta,y) {
+  gamma <- .00015                       #so z variance matches marginal y variance if sd is .01
+  ## mu is yield of riskless asset
+  mu <- .03
+  theta <- c(1 - sum(theta), theta)
+  y <- c(mu,y)
+  xy <- crossprod(theta, y)
+  u <- xy - .5 * xy^2 - .5 * gamma * crossprod(theta[-1])
   ## gamma is interpretable as the variance of an irreducible, zero-mean source of uncertainty
   ## in all assets except the first (risk free) asset.
   ## first version of this function applied it to all assets, and forgot the .5.
--- toolbox/RIplus/trunk/DiscreteX/xfcnCapm.R
+++ toolbox/RIplus/trunk/DiscreteX/xfcnCapm.R
@@ -1,20 +1,25 @@
 xfcnCapm <- function(ygivenx, y) {
-  gamma <- .001  # penalty on leverage, or irreducible component of variance in all assets but first
-  require(tensor)
-  ybar <- ygivenx %*% y
-  nx <- dim(ygivenx)[1]
-  ny <- dim(y)[1]
-  my <- dim(y)[2]
-  y2bar <- array(0, c(nx, my, my))
-  y2 <- array(0, c(ny, my, my))
-  for (iy in 1:ny) y2[iy, , ] <- y[iy, ] %o% y[iy, ]
-  y2bar <- tensor(ygivenx, y2, 2, 1)          # nx by my by my array
-  x <- matrix(0, nx, my)
-  for (ix in 1:nx) {
-    y2bari <- solve(y2bar[ix, , ] + gamma * diag(c(0, rep(1, my-1))))
-    ybarix <- ybar[ix,]
-    lmd <- -(1 - sum(y2bari %*%  ybarix))/sum(y2bari)
-    x[ix, ] <- y2bari %*% (ybarix - lmd)
-  }
-  return(x)
+    mu <- .03                           # return on riskless asset
+    gamma <- .00015  # irreducible component of variance in all assets but first
+    ## ygivenx and y are for the risky assets only.  We fill out the riskless
+    ## asset at the start here.
+    require(tensor)
+    nx <- dim(ygivenx)[1]
+    ny <- dim(y)[1]
+    y <- cbind(rep(mu, ny), y)
+    my <- dim(y)[2]
+    ybar <- ygivenx %*% y
+    y2bar <- array(0, c(nx, my, my))
+    y2 <- array(0, c(ny, my, my))
+    for (iy in 1:ny)
+        y2[iy, , ] <- y[iy, ] %o% y[iy, ]
+    y2bar <- tensor(ygivenx, y2, 2, 1)          # nx by my by my array
+    x <- matrix(0, nx, my)
+    for (ix in 1:nx) {
+        y2bari <- solve(y2bar[ix, , ] + gamma * diag(c(0, rep(1, my-1))))
+        ybarix <- ybar[ix, ]
+        lmd <- -(1 - sum(y2bari %*%  ybarix))/sum(y2bari)
+        x[ix, ] <- y2bari %*% (ybarix - lmd)
+    }
+    return(x[ , -1])                      #leave out riskless weight
 }
